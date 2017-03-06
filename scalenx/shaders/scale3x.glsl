c0e20564195c0d1c09e78d88ddb629002d678bc6
hunterk <hunter_kaller@yahoo.com>
2017-03-06 15:23:27 -0600

    add scalenx manual conversions

---
+++ scalenx/shaders/scale3x.glsl
@@ -0,0 +1,178 @@
+/*
+	Scale3x
+
+Filter:	Nearest
+Scale:	3x
+
+Scale3x is real-time graphics effect able to increase the size of small bitmaps guessing the missing pixels without blurring the images.
+It was originally developed for the AdvanceMAME project in the year 2001 to improve the quality of old games with a low video resolution.
+
+Homepage: http://scale2x.sourceforge.net/
+Copyright (C) 2001, 2002, 2003, 2004 Andrea Mazzoleni 
+License: GNU-GPL  
+
+*/
+
+#define saturate(c) clamp(c, 0.0, 1.0)
+#define lerp(a,b,c) mix(a,b,c)
+#define mul(a,b) (b*a)
+#define fmod(c,d) mod(c,d)
+#define frac(c) fract(c)
+#define tex2D(c,d) texture(c,d)
+#define float2 vec2
+#define float3 vec3
+#define float4 vec4
+#define int2 ivec2
+#define int3 ivec3
+#define int4 ivec4
+#define bool2 bvec2
+#define bool3 bvec3
+#define bool4 bvec4
+#define float2x2 mat2x2
+#define float3x3 mat3x3
+#define float4x4 mat4x4
+
+#define decal Source
+
+bool eq(float3 A, float3 B){
+	return (A==B);
+}
+
+bool neq(float3 A, float3 B){
+	return (A!=B);
+}
+
+#if defined(VERTEX)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING out
+#define COMPAT_ATTRIBUTE in
+#define COMPAT_TEXTURE texture
+#else
+#define COMPAT_VARYING varying 
+#define COMPAT_ATTRIBUTE attribute 
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+COMPAT_ATTRIBUTE vec4 VertexCoord;
+COMPAT_ATTRIBUTE vec4 COLOR;
+COMPAT_ATTRIBUTE vec4 TexCoord;
+COMPAT_VARYING vec4 COL0;
+COMPAT_VARYING vec4 TEX0;
+COMPAT_VARYING vec4 t1;
+COMPAT_VARYING vec4 t2;
+COMPAT_VARYING vec4 t3;
+
+uniform mat4 MVPMatrix;
+uniform int FrameDirection;
+uniform int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+
+// vertex compatibility #defines
+#define vTexCoord TEX0.xy
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
+#define outsize vec4(OutputSize, 1.0 / OutputSize)
+
+void main()
+{
+    gl_Position = MVPMatrix * VertexCoord;
+    COL0 = COLOR;
+    TEX0.xy = TexCoord.xy;
+	
+	float2 ps = float2(SourceSize.z, SourceSize.w);
+	float dx = ps.x;
+	float dy = ps.y;
+
+	t1 = TEX0.xxxy + float4(-dx,  0, dx,-dy);	// A, B, C
+	t2 = TEX0.xxxy + float4(-dx,  0, dx,  0);	// D, E, F
+	t3 = TEX0.xxxy + float4(-dx,  0, dx, dy);	// G, H, I
+}
+
+#elif defined(FRAGMENT)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING in
+#define COMPAT_TEXTURE texture
+out vec4 FragColor;
+#else
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+precision mediump float;
+#endif
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+uniform int FrameDirection;
+uniform int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+uniform sampler2D Texture;
+COMPAT_VARYING vec4 TEX0;
+COMPAT_VARYING vec4 t1;
+COMPAT_VARYING vec4 t2;
+COMPAT_VARYING vec4 t3;
+
+// fragment compatibility #defines
+#define Source Texture
+#define vTexCoord TEX0.xy
+#define texture(c, d) COMPAT_TEXTURE(c, d)
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
+#define outsize vec4(OutputSize, 1.0 / OutputSize)
+
+void main()
+{
+	// subpixel determination
+	float2 fp = floor(3.0 * frac(vTexCoord*SourceSize.xy));
+
+	/*
+		A B C		E0 E1 E2
+		D E F		E3 E4 E5
+		G H I		E6 E7 E8
+	*/
+
+	// reading the texels
+	float3 A = tex2D(decal, t1.xw).xyz;
+	float3 B = tex2D(decal, t1.yw).xyz;
+	float3 C = tex2D(decal, t1.zw).xyz;
+	float3 D = tex2D(decal, t2.xw).xyz;
+	float3 E = tex2D(decal, t2.yw).xyz;
+	float3 F = tex2D(decal, t2.zw).xyz;
+	float3 G = tex2D(decal, t3.xw).xyz;
+	float3 H = tex2D(decal, t3.yw).xyz;
+	float3 I = tex2D(decal, t3.zw).xyz;
+
+	// equality checks
+	bool eqBD = eq(B,D), eqBF = eq(B,F), eqHD = eq(H,D), eqHF = eq(H,F), neqEA = neq(E,A), neqEC = neq(E,C), neqEG = neq(E,G), neqEI = neq(E,I); 
+
+	// rules
+	float3 E0 = eqBD ? B : E;
+	float3 E1 = eqBD && neqEC || eqBF && neqEA ? B : E;
+	float3 E2 = eqBF ? B : E;
+	float3 E3 = eqBD && neqEG || eqHD && neqEA ? D : E;
+	float3 E5 = eqBF && neqEI || eqHF && neqEC ? F : E;
+	float3 E6 = eqHD ? H : E;
+	float3 E7 = eqHD && neqEI || eqHF && neqEG ? H : E;
+	float3 E8 = eqHF ? H : E;
+
+	// general condition & subpixel output
+	FragColor = vec4(neq(B,H) && neq(D,F) ? (fp.y == 0 ? (fp.x == 0 ? E0 : fp.x == 1 ? E1 : E2) : (fp.y == 1 ? (fp.x == 0 ? E3 : fp.x == 1 ? E : E5) : (fp.x == 0 ? E6 : fp.x == 1 ? E7 : E8))) : E, 1.0);
+} 
+#endif
---
+++ scalenx/shaders/scale3xSFX.glsl
@@ -0,0 +1,230 @@
+/*
+	Scale3xSFX
+	by Sp00kyFox, 2015
+
+Filter:	Nearest
+Scale:	3x
+
+Scale3SFX improves upon the original Scale3x by avoiding the occurence of artifacts and smoothing out 45° edges.
+
+*/
+
+// Compatibility #ifdefs needed for parameters
+#ifdef GL_ES
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+// Parameter lines go here:
+#pragma parameter YTR "SCALE2xSFX Y Threshold" 48.0 0.0 255.0 1.0
+#pragma parameter UTR "SCALE2xSFX U Threshold"  7.0 0.0 255.0 1.0
+#pragma parameter VTR "SCALE2xSFX V Threshold"  6.0 0.0 255.0 1.0
+#ifdef PARAMETER_UNIFORM
+// All parameter floats need to have COMPAT_PRECISION in front of them
+uniform COMPAT_PRECISION float YTR;
+uniform COMPAT_PRECISION float UTR;
+uniform COMPAT_PRECISION float VTR;
+#else
+#define YTR 48.0
+#define UTR 7.0
+#define VTR 6.0
+#endif
+
+#define saturate(c) clamp(c, 0.0, 1.0)
+#define lerp(a,b,c) mix(a,b,c)
+#define mul(a,b) (b*a)
+#define fmod(c,d) mod(c,d)
+#define frac(c) fract(c)
+#define tex2D(c,d) texture(c,d)
+#define float2 vec2
+#define float3 vec3
+#define float4 vec4
+#define int2 ivec2
+#define int3 ivec3
+#define int4 ivec4
+#define bool2 bvec2
+#define bool3 bvec3
+#define bool4 bvec4
+#define float2x2 mat2x2
+#define float3x3 mat3x3
+#define float4x4 mat4x4
+#define float4x3 mat4x3
+
+#define decal Source
+
+const float3x3 YUV  = float3x3(0.299, -0.168736, 0.5, 0.587, -0.331264, -0.418688, 0.114, 0.5, -0.081312);	// transponed
+float3 thresh = float3(YTR, UTR, VTR)/255.0;
+
+bool3 eq(float3 A, float3 B){
+	return lessThanEqual(abs(A-B) , thresh);
+}
+
+bool3 neq(float3 A, float3 B){
+	return greaterThan(abs(A-B) , thresh);
+}
+
+#if defined(VERTEX)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING out
+#define COMPAT_ATTRIBUTE in
+#define COMPAT_TEXTURE texture
+#else
+#define COMPAT_VARYING varying 
+#define COMPAT_ATTRIBUTE attribute 
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+COMPAT_ATTRIBUTE vec4 VertexCoord;
+COMPAT_ATTRIBUTE vec4 COLOR;
+COMPAT_ATTRIBUTE vec4 TexCoord;
+COMPAT_VARYING vec4 COL0;
+COMPAT_VARYING vec4 TEX0;
+COMPAT_VARYING vec4 t1;
+COMPAT_VARYING vec4 t2;
+COMPAT_VARYING vec4 t3;
+COMPAT_VARYING vec4 t4;
+COMPAT_VARYING vec4 t5;
+
+uniform mat4 MVPMatrix;
+uniform int FrameDirection;
+uniform int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+
+// vertex compatibility #defines
+#define vTexCoord TEX0.xy
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
+#define outsize vec4(OutputSize, 1.0 / OutputSize)
+
+void main()
+{
+    gl_Position = MVPMatrix * VertexCoord;
+    COL0 = COLOR;
+    TEX0.xy = TexCoord.xy;
+
+	float2 ps = float2(SourceSize.z, SourceSize.w);
+	float dx = ps.x;
+	float dy = ps.y;
+
+	t1 = TEX0.xxxy + float4(-dx,  0, dx,-dy);	// A, B, C
+	t2 = TEX0.xxxy + float4(-dx,  0, dx,  0);	// D, E, F
+	t3 = TEX0.xxxy + float4(-dx,  0, dx, dy);	// G, H, I
+	t4 = TEX0.xyxy + float4(    0,-2*dy,-2*dx,    0);	// J, K
+	t5 = TEX0.xyxy + float4( 2*dx,    0,    0, 2*dy);	// L, M
+}
+
+#elif defined(FRAGMENT)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING in
+#define COMPAT_TEXTURE texture
+out vec4 FragColor;
+#else
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+precision mediump float;
+#endif
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+uniform int FrameDirection;
+uniform int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+uniform sampler2D Texture;
+COMPAT_VARYING vec4 TEX0;
+COMPAT_VARYING vec4 t1;
+COMPAT_VARYING vec4 t2;
+COMPAT_VARYING vec4 t3;
+COMPAT_VARYING vec4 t4;
+COMPAT_VARYING vec4 t5;
+
+// fragment compatibility #defines
+#define Source Texture
+#define vTexCoord TEX0.xy
+#define texture(c, d) COMPAT_TEXTURE(c, d)
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
+#define outsize vec4(OutputSize, 1.0 / OutputSize)
+
+void main()
+{
+	// subpixel determination
+	float2 fp = floor(2.0 * frac(vTexCoord*SourceSize.xy));
+
+	/*
+		    J
+		  A B C		E0 E1
+		K D E F L	E2 E3
+		  G H I
+		    M
+	*/
+
+	// reading the texels & colorspace conversion
+	float3 b = tex2D(decal, t1.yw).xyz;
+	float3 d = tex2D(decal, t2.xw).xyz;
+	float3 e = tex2D(decal, t2.yw).xyz;
+	float3 f = tex2D(decal, t2.zw).xyz;
+	float3 h = tex2D(decal, t3.yw).xyz;
+
+	float4x3 tmp = mul(float4x3(b,d,e,f), YUV);
+	float3 B = tmp[0], D = tmp[1], E = tmp[2], F = tmp[3], H = mul(h, YUV);
+
+	float3 A = tex2D(decal, t1.xw).xyz;
+	float3 C = tex2D(decal, t1.zw).xyz;
+	float3 G = tex2D(decal, t3.xw).xyz;
+	float3 I = tex2D(decal, t3.zw).xyz;
+
+	tmp = mul(float4x3(A,C,G,I), YUV);
+	A = tmp[0], C = tmp[1], G = tmp[2], I = tmp[3];
+
+	float3 J = tex2D(decal, t4.xy).xyz;
+	float3 K = tex2D(decal, t4.zw).xyz;
+	float3 L = tex2D(decal, t5.xy).xyz;
+	float3 M = tex2D(decal, t5.zw).xyz;
+
+	tmp = mul(float4x3(J,K,L,M), YUV);
+	J = tmp[0], K = tmp[1], L = tmp[2], M = tmp[3];
+
+	// parent condition
+	bool par0 = neq(B,F) == bool3(true) && neq(D,H) == bool3(true);
+	bool par1 = neq(B,D) == bool3(true) && neq(F,H) == bool3(true);
+
+	// equality checks
+	bool AE = eq(A,E) == bool3(true);
+	bool CE = eq(C,E) == bool3(true);
+	bool EG = eq(E,G) == bool3(true);
+	bool EI = eq(E,I) == bool3(true);
+
+	// artifact prevention
+	bool art0 = CE || EG;
+	bool art1 = AE || EI;
+
+	// rules
+	float3 E0 = eq(B,D) == bool3(true) && par0 == true && (AE == false || art0 == true || eq(A,J) == bool3(true) || eq(A,K) == bool3(true)) ? 0.5*(b+d) : e;
+	float3 E1 = eq(B,F) == bool3(true) && par1 == true && (CE == false || art1 == true || eq(C,J) == bool3(true) || eq(C,L) == bool3(true)) ? 0.5*(b+f) : e;
+	float3 E2 = eq(D,H) == bool3(true) && par1 == true && (EG == false || art1 == true || eq(G,K) == bool3(true) || eq(G,M) == bool3(true)) ? 0.5*(h+d) : e;
+	float3 E3 = eq(F,H) == bool3(true) && par0 == true && (EI == false || art0 == true || eq(I,L) == bool3(true) || eq(I,M) == bool3(true)) ? 0.5*(h+f) : e;
+
+	// subpixel output
+	FragColor = vec4(fp.y == 0 ? (fp.x == 0 ? E0 : E1) : (fp.x == 0 ? E2 : E3), 1.0);
+} 
+#endif
