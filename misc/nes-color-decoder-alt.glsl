cccca1310cd94aac2b448a7a06855ea2e696c3f4
Monroe88 <wmsmart88@gmail.com>
2017-04-20 22:49:47 -0500

    (nes-color-decoder) Remove OutputSize macro

--- misc/nes-color-decoder-alt.glsl
+++ misc/nes-color-decoder-alt.glsl
@@ -170,7 +170,6 @@ COMPAT_VARYING vec4 TEX0;
 #define vTexCoord TEX0.xy
 #define texture(c, d) COMPAT_TEXTURE(c, d)
 #define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
-#define OutputSize vec4(OutputSize, 1.0 / OutputSize)
 
 void main()
 {
