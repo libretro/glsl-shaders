9a872204e500e3600c32a384ccfe2b0ab561ace5
hunterk <hunter_kaller@yahoo.com>
2017-06-15 22:10:31 -0500

    fix crt-geom black screen on SMS

--- crt/shaders/crt-geom.glsl
+++ crt/shaders/crt-geom.glsl
@@ -199,7 +199,7 @@ void main()
 	cosangle = cos(vec2(x_tilt, y_tilt)) + vec2(0.001);//cos(vec2(max(abs(x_tilt), 1e-3), max(abs(y_tilt), 1e-3)));
 	stretch = maxscale();
 
-	ilfac = vec2(1.0,floor(InputSize.y/200.0));
+	ilfac = vec2(1.0,clamp(floor(InputSize.y/200.0), 1.0, 2.0));
 
 // The size of one texel, in texture-coordinates.
 	vec2 sharpTextureSize = vec2(SHARPER * TextureSize.x, TextureSize.y);
