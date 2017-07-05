70cdd073a7f6e18bae6eaee6f4b3666b87ecc77f
hunterk <hunter_kaller@yahoo.com>
2017-07-04 19:57:43 -0500

    add scanline-fract

---
+++ misc/scanline-fract.glsl
@@ -0,0 +1,130 @@
+// Super-basic scanline shader
+// (looks bad at non-integer scales)
+// by hunterk
+// license: public domain
+
+// Parameter lines go here:
+#pragma parameter THICKNESS "Scanline Thickness" 2.0 1.0 12.0 1.0
+#pragma parameter DARKNESS "Scanline Darkness" 0.50 0.0 1.0 0.05
+#pragma parameter BRIGHTBOOST "Scanline Boost Bright" 1.1 1.0 1.2 0.1
+
+#if defined(VERTEX)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING out
+#define COMPAT_ATTRIBUTE in
+#define COMPAT_TEXTURE texture
+#else
+#define COMPAT_VARYING varying 
+#define COMPAT_ATTRIBUTE attribute 
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+COMPAT_ATTRIBUTE vec4 VertexCoord;
+COMPAT_ATTRIBUTE vec4 COLOR;
+COMPAT_ATTRIBUTE vec4 TexCoord;
+COMPAT_VARYING vec4 COL0;
+COMPAT_VARYING vec4 TEX0;
+
+vec4 _oPosition1; 
+uniform mat4 MVPMatrix;
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+
+// compatibility #defines
+#define vTexCoord TEX0.xy
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
+#define OutSize vec4(OutputSize, 1.0 / OutputSize)
+
+void main()
+{
+    gl_Position = MVPMatrix * VertexCoord;
+    TEX0.xy = TexCoord.xy * 1.0004;
+}
+
+#elif defined(FRAGMENT)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING in
+#define COMPAT_TEXTURE texture
+out vec4 FragColor;
+#else
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+precision mediump float;
+#endif
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+uniform sampler2D Texture;
+COMPAT_VARYING vec4 TEX0;
+
+// compatibility #defines
+#define Source Texture
+#define vTexCoord TEX0.xy
+#define texture(c, d) COMPAT_TEXTURE(c, d)
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
+#define OutSize vec4(OutputSize, 1.0 / OutputSize)
+
+#ifdef PARAMETER_UNIFORM
+// All parameter floats need to have COMPAT_PRECISION in front of them
+uniform COMPAT_PRECISION float THICKNESS;
+uniform COMPAT_PRECISION float DARKNESS;
+uniform COMPAT_PRECISION float BRIGHTBOOST;
+#else
+#define THICKNESS 1.0
+#define DARKNESS 0.5
+#define BRIGHTBOOST 1.1
+#endif
+
+vec3 RGBtoYIQ(vec3 RGB){
+	const mat3 yiqmat = mat3(
+		0.2989, 0.5870, 0.1140,
+		0.5959, -0.2744, -0.3216,
+		0.2115, -0.5229, 0.3114);
+	return RGB * yiqmat;
+}
+
+vec3 YIQtoRGB(vec3 YIQ){
+	const mat3 rgbmat = mat3(
+		1.0, 0.956, 0.6210,
+		1.0, -0.2720, -0.6474,
+		1.0, -1.1060, 1.7046);
+	return YIQ * rgbmat;
+}
+
+void main()
+{
+	float lines = fract(vTexCoord.y * SourceSize.y);
+	float scale_factor = floor((OutputSize.y / InputSize.y) + 0.4999);
+	vec4 screen = texture(Source, vTexCoord);
+	screen.rgb = RGBtoYIQ(screen.rgb);
+	screen.r *= BRIGHTBOOST;
+	screen.rgb = YIQtoRGB(screen.rgb);
+
+    FragColor = (lines > (1.0 / scale_factor * THICKNESS)) ? screen : screen * vec4(1.0 - DARKNESS);
+} 
+#endif
