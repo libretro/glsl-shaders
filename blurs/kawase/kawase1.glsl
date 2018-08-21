55be415718ccf3a35859dd8798498409039e8e11
hunterk <hunter_kaller@yahoo.com>
2018-08-21 10:19:28 -0500

    add kawase blur

---
+++ blurs/kawase/kawase1.glsl
@@ -0,0 +1,112 @@
+// Kuwase Blur
+// based on a shadertoy by Kubuxu
+// https://www.shadertoy.com/view/Xl3XW7
+
+#if defined(VERTEX)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING out
+#define COMPAT_ATTRIBUTE in
+#define COMPAT_TEXTURE texture
+#else
+#define COMPAT_VARYING varying 
+#define COMPAT_ATTRIBUTE attribute 
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+COMPAT_ATTRIBUTE vec4 VertexCoord;
+COMPAT_ATTRIBUTE vec4 COLOR;
+COMPAT_ATTRIBUTE vec4 TexCoord;
+COMPAT_VARYING vec4 COL0;
+COMPAT_VARYING vec4 TEX0;
+
+vec4 _oPosition1; 
+uniform mat4 MVPMatrix;
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+
+// compatibility #defines
+#define vTexCoord TEX0.xy
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
+#define OutSize vec4(OutputSize, 1.0 / OutputSize)
+
+void main()
+{
+    gl_Position = MVPMatrix * VertexCoord;
+    TEX0.xy = TexCoord.xy;
+}
+
+#elif defined(FRAGMENT)
+
+#ifdef GL_ES
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+precision mediump float;
+#endif
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING in
+#define COMPAT_TEXTURE texture
+out COMPAT_PRECISION vec4 FragColor;
+#else
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
+#define COMPAT_TEXTURE texture2D
+#endif
+
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+uniform sampler2D Texture;
+COMPAT_VARYING vec4 TEX0;
+
+// compatibility #defines
+#define Source Texture
+#define vTexCoord TEX0.xy
+
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
+#define OutSize vec4(OutputSize, 1.0 / OutputSize)
+
+vec4 reSample(float d, vec2 uv, sampler2D decal)
+{
+ 
+    vec2 step1 = (vec2(d) + 0.5) * SourceSize.zw;
+    vec4 color = vec4(0.);
+    color += texture(decal, uv + step1) / 4.;
+    color += texture(decal,  uv - step1) / 4.;
+  	vec2 step2 = step1;
+    step2.x = -step2.x;
+    color += texture(decal, uv + step2) / 4.;
+    color += texture(decal,  uv - step2) / 4.;
+    return color;
+}
+
+void main()
+{
+   vec2 texcoord;
+#ifdef GL_ES
+   texcoord = vTexCoord.xy;
+#else
+   // avoid sampling outside of the image (causes dark border)
+   vec2 offset = InputSize.xy / TextureSize.xy;
+   texcoord.xy = clamp(vTexCoord, vec2(0.01 * offset), vec2(0.99 * offset));
+#endif
+   FragColor = reSample(1., texcoord, Source);
+} 
+#endif
