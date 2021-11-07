b52854f6f6d9aa9aaa84699cfa6cc5f361d2fc16
metallic77 <43163462+metallic77@users.noreply.github.com>
2021-11-07 20:20:24 +0200

    add masks from crt-guest.r-dr.venom

--- crt/shaders/crt-guest-cgwg-curvature.glsl
+++ crt/shaders/crt-guest-cgwg-curvature.glsl
@@ -27,7 +27,11 @@
 #pragma parameter beam_max "Scanline bright" 2.1 0.5 3.0 0.05
 #pragma parameter h_sharp "Horizontal sharpness" 2.00 1.0 5.0 0.05
 #pragma parameter gamma_out "Gamma out" 0.5 0.2 0.6 0.01
+#pragma parameter shadowMask "CRT Mask: 0:CGWG, 1-4:Lottes, 5-6:'Trinitron'" 0.0 -1.0 7.0 1.0
+#pragma parameter masksize "CRT Mask Size (2.0 is nice in 4k)" 1.0 1.0 2.0 1.0
+#pragma parameter mcut "Mask 5-7 cutoff" 0.2 0.0 0.5 0.05
 #pragma parameter maskDark "Lottes maskDark" 0.5 0.0 2.0 0.1
+#pragma parameter maskLight "Lottes maskLight" 1.5 0.0 2.0 0.1
 #pragma parameter CGWG "CGWG Mask Str." 0.4 0.0 1.0 0.1
 #pragma parameter warpX "warpX" 0.0 0.0 0.125 0.01
 #pragma parameter warpY "warpY" 0.0 0.0 0.125 0.01
@@ -118,7 +122,11 @@ uniform COMPAT_PRECISION float beam_min;
 uniform COMPAT_PRECISION float beam_max;
 uniform COMPAT_PRECISION float h_sharp;
 uniform COMPAT_PRECISION float gamma_out;
+uniform COMPAT_PRECISION float shadowMask;
+uniform COMPAT_PRECISION float masksize;
+uniform COMPAT_PRECISION float mcut;
 uniform COMPAT_PRECISION float maskDark;
+uniform COMPAT_PRECISION float maskLight;
 uniform COMPAT_PRECISION float CGWG;
 uniform COMPAT_PRECISION float warpX;
 uniform COMPAT_PRECISION float warpY;
@@ -126,11 +134,15 @@ uniform COMPAT_PRECISION float warpY;
 #define brightboost  1.20     // adjust brightness
 #define saturation   1.10     // 1.0 is normal saturation
 #define scanline     8.0      // scanline param, vertical sharpness
-#define beam_min     1.70	  // dark area beam min - wide
-#define beam_max     2.10 	  // bright area beam max - narrow
+#define beam_min     1.70     // dark area beam min - wide
+#define beam_max     2.10     // bright area beam max - narrow
 #define h_sharp      1.25     // pixel sharpness
 #define gamma_out    0.50     // output gamma
+#define shadowMask   0.0      // Shadow mask type 
+#define masksize     1.0      // Shadow mask size
+#define mcut         0.20     // Mask 5&6 cutoff
 #define maskDark     0.50     // Dark "Phosphor"
+#define maskLight    1.50     // Light "Phosphor"
 #define CGWG         0.40     // CGWG Mask Strength
 #define warpX        0.0    // Curvature X
 #define warpY        0.0    // Curvature Y
@@ -145,21 +157,122 @@ float sw(float x, float l)
 	return d;
 }
 
-vec3 Mask(vec2 pos)
+// Shadow mask (1-4 from PD CRT Lottes shader).
+vec3 Mask(vec2 pos, vec3 c)
 {
-   vec3 mask = vec3(maskDark, maskDark, maskDark);
-
-   // Phosphor.
-  
-   {
-      float mf = floor(mod(pos.x,2.0));
-      float mc = 1.0 - CGWG;	
-      if (mf == 0.0) { mask.r = 1.0; mask.g = mc; mask.b = 1.0; }
-      else { mask.r = mc; mask.g = 1.0; mask.b = mc; };
-   }    
+	pos = floor(pos/masksize);
+	vec3 mask = vec3(maskDark, maskDark, maskDark);
+	
+	// No mask
+	if (shadowMask == -1.0)
+	{
+		mask = vec3(1.0);
+	}       
+	
+	// Phosphor.
+	else if (shadowMask == 0.0)
+	{
+		pos.x = fract(pos.x*0.5);
+		float mc = 1.0 - CGWG;
+		if (pos.x < 0.5) { mask.r = 1.1; mask.g = mc; mask.b = 1.1; }
+		else { mask.r = mc; mask.g = 1.1; mask.b = mc; }
+	}    
    
-      return mask;
-} 
+	// Very compressed TV style shadow mask.
+	else if (shadowMask == 1.0)
+	{
+		float line = maskLight;
+		float odd  = 0.0;
+
+		if (fract(pos.x/6.0) < 0.5)
+			odd = 1.0;
+		if (fract((pos.y + odd)/2.0) < 0.5)
+			line = maskDark;
+
+		pos.x = fract(pos.x/3.0);
+    
+		if      (pos.x < 0.333) mask.r = maskLight;
+		else if (pos.x < 0.666) mask.g = maskLight;
+		else                    mask.b = maskLight;
+		
+		mask*=line;  
+	} 
+
+	// Aperture-grille.
+	else if (shadowMask == 2.0)
+	{
+		pos.x = fract(pos.x/3.0);
+
+		if      (pos.x < 0.333) mask.r = maskLight;
+		else if (pos.x < 0.666) mask.g = maskLight;
+		else                    mask.b = maskLight;
+	} 
+
+	// Stretched VGA style shadow mask (same as prior shaders).
+	else if (shadowMask == 3.0)
+	{
+		pos.x += pos.y*3.0;
+		pos.x  = fract(pos.x/6.0);
+
+		if      (pos.x < 0.333) mask.r = maskLight;
+		else if (pos.x < 0.666) mask.g = maskLight;
+		else                    mask.b = maskLight;
+	}
+
+	// VGA style shadow mask.
+	else if (shadowMask == 4.0)
+	{
+		pos.xy = floor(pos.xy*vec2(1.0, 0.5));
+		pos.x += pos.y*3.0;
+		pos.x  = fract(pos.x/6.0);
+
+		if      (pos.x < 0.333) mask.r = maskLight;
+		else if (pos.x < 0.666) mask.g = maskLight;
+		else                    mask.b = maskLight;
+	}
+	
+	// Alternate mask 5
+	else if (shadowMask == 5.0)
+	{
+		float mx = max(max(c.r,c.g),c.b);
+		vec3 maskTmp = vec3( min( 1.25*max(mx-mcut,0.0)/(1.0-mcut) ,maskDark + 0.2*(1.0-maskDark)*mx));
+		float adj = 0.80*maskLight - 0.5*(0.80*maskLight - 1.0)*mx + 0.75*(1.0-mx);	
+		mask = maskTmp;
+		pos.x = fract(pos.x/2.0);
+		if  (pos.x < 0.5)
+		{	mask.r  = adj;
+			mask.b  = adj;
+		}
+		else     mask.g = adj;
+	}    
+
+	// Alternate mask 6
+	else if (shadowMask == 6.0)
+	{
+		float mx = max(max(c.r,c.g),c.b);
+		vec3 maskTmp = vec3( min( 1.33*max(mx-mcut,0.0)/(1.0-mcut) ,maskDark + 0.225*(1.0-maskDark)*mx));
+		float adj = 0.80*maskLight - 0.5*(0.80*maskLight - 1.0)*mx + 0.75*(1.0-mx);
+		mask = maskTmp;
+		pos.x = fract(pos.x/3.0);
+		if      (pos.x < 0.333) mask.r = adj;
+		else if (pos.x < 0.666) mask.g = adj;
+		else                    mask.b = adj; 
+	}
+	
+	// Alternate mask 7
+	else if (shadowMask == 7.0)
+	{
+		float mc = 1.0 - CGWG;
+		float mx = max(max(c.r,c.g),c.b);
+		float maskTmp = min(1.6*max(mx-mcut,0.0)/(1.0-mcut) , mc);
+		mask = vec3(maskTmp);
+		pos.x = fract(pos.x/2.0);
+		if  (pos.x < 0.5) mask = vec3(1.0 + 0.6*(1.0-mx));
+	}    
+	
+	return mask;
+}  
+
 
 // Distortion of scanlines, and end of screen alpha.
 vec2 Warp(vec2 pos)
@@ -205,10 +318,11 @@ void main()
 	color = min(color, 1.0);
 
 	color = pow(color, vec3(gamma_out, gamma_out, gamma_out));
-	
+	vec3 mcolor = color;
+
 	float l = length(color);
 	color = normalize(pow(color, vec3(saturation,saturation,saturation)))*l;
-	color = color*Mask(gl_FragCoord.xy);	
+	color = color*Mask(gl_FragCoord.xy*1.0001, mcolor);
 
     FragColor = vec4(color, 1.0);
 } 
