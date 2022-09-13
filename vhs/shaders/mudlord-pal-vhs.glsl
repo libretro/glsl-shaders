97d18f2519b62e4c8702d62f72b91afc6bacadd0
mudlord <mudlord@protonmail.com>
2022-09-14 08:59:16 +1000

    It's been a long time. We shouldn't of left you. Without a dope beat to step to. Time's up.

--- vhs/shaders/mudlord-pal-vhs.glsl
+++ vhs/shaders/mudlord-pal-vhs.glsl
@@ -103,8 +103,90 @@ uniform COMPAT_PRECISION    float border;
 #define border 1.0
 #endif
 
-float f(float e){vec2 f=vec2(e,e);float v=12.9898,r=78.233,t=43758.5,S=dot(f.rg,vec2(v,r)),b=mod(S,3.14);return fract(sin(b)*t);}vec2 e(vec2 e){return e=(e-.5)*2.,e*=1.1,e.r*=1.+pow(abs(e.g)/5.,2.),e.g*=1.+pow(abs(e.r)/4.,2.),e=e/2.+.5,e=e*.92+.04,e;}void main(){vec2 v=vTexCoord.rg*TextureSize.rg/InputSize.rg,r=v;r=mix(r,e(r),1.)*InputSize.rg/TextureSize.rg;vec4 c=vec4(0.);vec2 m=r,t=m;mat3 b=mat3(.299,-.147,.615,.587,-.289,-.515,.114,.436,-.1),g=mat3(1.,1.,1.,0.,-.395,2.032,1.14,-.581,0.);float F=1.;if(TVNoise==1.)F-=f(m.r*FrameCount*.1+m.g*FrameCount*50.+FrameCount)*.5;if(PALSignal==1.){vec3 i=vec3(0.);float O=.3,S=-.002;for(int a=10;a>=0;a-=1){float s=float(a)/10.;if(s<0.)s=0.;float p=s*-.05*O+S,l=s*.1*O+S;vec3 T=(vec3(1.)-pow(vec3(s),vec3(.2,1.,1.)))*.2;vec2 o=t+vec2(p,0.),u=t+vec2(l,0.);i+=b*texture(Source,o).rgb*T;i+=b*texture(Source,u).rgb*T;}i.r=i.r*.2+(b*texture(Source,t).rgb).r*.8;c.rgb=g*i*F;}else c.rgb=texture(Source,t).rgb;if(phosphors==1.){float S=v.g*OutputSize.g*OutputSize.g/OutputSize.g;vec3 s=mix(vec3(1.,.7,1.),vec3(.7,1.,.7),floor(mod(S,2.)));c.rgb*=s;}if(border==1.){vec2 s=-1.+2.*v;float S=(1.-s.r*s.r)*(1.-s.g*s.g),u=clamp(frameSharpness*(pow(S,frameShape)-frameLimit),0.,1.);c.rgb*=u;}FragColor=vec4(c.rgb,1.);}
+float rand(float x)
+{
+    vec2 co = vec2(x,x);
+    float a = 12.9898;
+    float b = 78.233;
+    float c = 43758.5453;
+    float dt= dot(co.xy ,vec2(a,b));
+    float sn= mod(dt,3.14);
+    return fract(sin(sn) * c);
+}
 
+vec2 crt(vec2 uv)
+{
+	uv = (uv - 0.5) * 2.0;
+	uv *= 1.1;	
+	uv.x *= 1.0 + pow((abs(uv.y) / 5.0), 2.0);
+	uv.y *= 1.0 + pow((abs(uv.x) / 4.0), 2.0);
+	uv  = (uv / 2.0) + 0.5;
+	uv =  uv *0.92 + 0.04;
+	return uv;
+}
 
+void main()
+{
+    vec2 q = (vTexCoord.xy * TextureSize.xy / InputSize.xy);//fragCoord.xy / iResolution.xy;
+    vec2 uv = q;
+    uv = mix( uv, crt( uv ), 1.0 ) * InputSize.xy / TextureSize.xy;
+    vec4 col = vec4(0.0);
+    vec2 uv_q =uv;
+
+    vec2 uv_n = uv_q;
+    mat3 rgbtoyuv = mat3(0.299, -0.147,  0.615, 0.587, -0.289, -0.515, 0.114, 0.436, -0.100);
+	mat3 yuvtorgb = mat3(1.000, 1.000, 1.000, 0.000, -0.395, 2.032, 1.140, -0.581, 0.000);
+    float shade = 1.0;
+
+    if(TVNoise == 1.0)
+{
+shade -= rand((uv_q.x*FrameCount) * 0.1 + (uv_q.y*FrameCount) * 50.0 + FrameCount) * 0.5;
+}
+
+if(PALSignal == 1.0)
+{
+     vec3 yuv = vec3(0.0);
+	float fix = 0.3;
+	float lumadelay = -0.002;
+
+  for (int x = 10; x >= 0; x -= 1)
+  {
+       float xx = float(x) / 10.0;
+       if(xx < 0.0) xx = 0.0 ;
+       float x1 = (xx * -0.05)* fix + lumadelay;
+       float x2 = (xx * 0.1)* fix + lumadelay;
+       vec3 mult = (vec3(1.0) - pow(vec3(xx), vec3(0.2, 1.0, 1.0))) * 0.2;
+        vec2 uv1 = uv_n + vec2(x1,0.0);
+       vec2 uv2 = uv_n + vec2(x2,0.0);
+       yuv += (rgbtoyuv * texture(Source,uv1).rgb) * mult;
+       yuv += (rgbtoyuv * texture(Source,uv2).rgb) * mult;
+
+  }
+  yuv.r = yuv.r * 0.2 + (rgbtoyuv *  texture(Source,uv_n).rgb).r * 0.8;
+    col.rgb = yuvtorgb * yuv * shade;
+}
+else
+{
+ col.rgb = texture(Source,uv_n).rgb;
+}
+    
+
+   
+if(phosphors==1.0)
+{
+    float mod_factor = q.y * OutputSize.y * OutputSize.y / OutputSize.y;
+	vec3 dotMaskWeights = mix(vec3(1.0, 0.7, 1.0),vec3(0.7, 1.0, 0.7),floor(mod(mod_factor, 2.0)));
+    col.rgb*= dotMaskWeights;
+}
+ 
+
+if(border ==1.0)
+{
+    vec2 p=-1.0+2.0*q;
+	float f = (1.0- p.x *p.x) * (1.0-p.y *p.y);
+	float frame = clamp(frameSharpness * (pow(f, frameShape) - frameLimit), 0.0, 1.0);
+	col.rgb*=frame;
+}
+   FragColor = vec4(col.rgb, 1.0);
+} 
 #endif
-
