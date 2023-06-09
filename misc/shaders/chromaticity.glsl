eefc20df31db6c957f177f3491e15e2cce673a2c
metallic77 <43163462+metallic77@users.noreply.github.com>
2023-06-09 20:18:55 +0300

    Update chromaticity.glsl
    automate color modes

--- misc/shaders/chromaticity.glsl
+++ misc/shaders/chromaticity.glsl
@@ -16,29 +16,18 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>. 
    */
- 
-#pragma parameter Readme "READ GLSL FILE FOR VARIOUS COEFFS" 0.0 0.0 1.0 1.0 
-#pragma parameter CHROMA_A_X "Chromaticity R x" 0.670 0.0 1.0 0.001
-#pragma parameter CHROMA_A_Y "Chromaticity R y" 0.330 0.0 1.0 0.001
-#pragma parameter CHROMA_B_X "Chromaticity G x" 0.210 0.0 1.0 0.001
-#pragma parameter CHROMA_B_Y "Chromaticity G y" 0.710 0.0 1.0 0.001
-#pragma parameter CHROMA_C_X "Chromaticity B x" 0.140 0.0 1.0 0.001
-#pragma parameter CHROMA_C_Y "Chromaticity B y" 0.080 0.0 1.0 0.001
-#pragma parameter CHROMA_A_WEIGHT "Chromaticity R luminance weight" 0.299 0.0 1.0 0.01
-#pragma parameter CHROMA_B_WEIGHT "Chromaticity G luminance weight" 0.587 0.0 1.0 0.01
-#pragma parameter CHROMA_C_WEIGHT "Chromaticity B luminance weight" 0.114 0.0 1.0 0.01
-#pragma parameter CRT_TR0 "Transfer Function" 0.018 0.0 0.2 0.001
-#pragma parameter CRT_TR "Transfer Function" 0.099 0.0 0.2 0.001
-#pragma parameter CRT_TR2 "Transfer Function" 4.5 3.0 5.0 0.05
+
+
+#pragma parameter COLOR_MODE "SRGB,SMPTE C,REC709,BT2020,SMPTE240,NTSC1953,EBU" 0.0 0.0 6.0 1.0   
 #pragma parameter SCALE_W "Scale white point" 1.0 0.0 1.0 1.0
 #pragma parameter GAMMAIN "Gamma In" 2.4 1.0 4.0 0.05
 #pragma parameter GAMMAOUT "Gamma Out" 2.2 1.0 4.0 0.05
 
 
 /* 
+SRGB         used by most webcams and computer graphics. ***NOTE***: Gamma 2.4
 SMPTE-C/170M used by NTSC and PAL and by SDTV in general.
 REC709       used by HDTV in general.
-SRGB         used by most webcams and computer graphics. ***NOTE***: Gamma 2.4
 BT2020       used by Ultra-high definition television (UHDTV) and wide color gamut.
 SMPTE240     used during the early days of HDTV (1988-1998).
 NTSC1953     used by NTSC at 1953. 
@@ -47,8 +36,8 @@ EBU          used by PAL/SECAM in 1975. Identical to REC601.
 
 
 //			  RX   RY      GX     GY     BX      BY      RL    GL    BL     TR0    TR   TR2
-// SMPTE C 	0.630 0.340 / 0.310 0.595 / 0.155	0.070 / 0.299 0.587 0.114 / 0.018 0.099 4.5
 // REC709  	0.640 0.330 / 0.300 0.600 / 0.150	0.060 / 0.212 0.715 0.072 / 0.018 0.099 4.5
+// SMPTE C 	0.630 0.340 / 0.310 0.595 / 0.155	0.070 / 0.299 0.587 0.114 / 0.018 0.099 4.5
 // SRGB  	0.640 0.330 / 0.300 0.600 / 0.150	0.060 / 0.299 0.587 0.114 / 0.040 0.055 12.92
 // BT2020   0.708 0.292 / 0.170 0.797 / 0.131   0.046 / 0.262 0.678 0.059 / 0.059 0.099 4.5
 // SMPTE240 0.630 0.340 / 0.310 0.595 / 0.155   0.070 / 0.212 0.701 0.086 / 0.091 0.111 4.0
@@ -94,38 +83,19 @@ uniform vec2 OutputSize;
 
 
 #ifdef PARAMETER_UNIFORM
-uniform COMPAT_PRECISION	float CHROMA_A_X;
-uniform COMPAT_PRECISION	float CHROMA_A_Y;
-uniform COMPAT_PRECISION	float CHROMA_B_X;
-uniform COMPAT_PRECISION	float CHROMA_B_Y;
-uniform COMPAT_PRECISION	float CHROMA_C_X;
-uniform COMPAT_PRECISION	float CHROMA_C_Y;
-uniform COMPAT_PRECISION	float CHROMA_A_WEIGHT;
-uniform COMPAT_PRECISION	float CHROMA_B_WEIGHT;
-uniform COMPAT_PRECISION	float CHROMA_C_WEIGHT;
+uniform COMPAT_PRECISION float COLOR_MODE;
 uniform COMPAT_PRECISION	float SCALE_W;
-uniform COMPAT_PRECISION	float CRT_TR0;
-uniform COMPAT_PRECISION	float CRT_TR;
-uniform COMPAT_PRECISION	float CRT_TR2;
+
 uniform COMPAT_PRECISION	float GAMMAIN;
 uniform COMPAT_PRECISION	float GAMMAOUT;
 
 #else
 
+#define COLOR_MODE 0.0
 #define CS 0.0
-#define  CHROMA_A_X 0.63
-#define  CHROMA_A_Y 0.34 
-#define  CHROMA_B_X 0.31
-#define  CHROMA_B_Y 0.595
-#define  CHROMA_C_X 0.155
-#define  CHROMA_C_Y 0.07
-#define  CHROMA_A_WEIGHT 0.2124
-#define  CHROMA_B_WEIGHT 0.7011
-#define  CHROMA_C_WEIGHT 0.0866
+
+
 #define  SCALE_W 1.0
-#define  CRT_TR0 0.018
-#define  CRT_TR 0.099
-#define  CRT_TR2 4.5
 #define  GAMMAIN 2.4
 #define  GAMMAOUT 2.25
 #endif
@@ -157,6 +127,54 @@ vec3 xyY_to_XYZ(const vec3 xyY)
 
 vec3 Yrgb_to_RGB(mat3 toRGB, vec3 W, vec3 Yrgb)
 {
+
+//0 SRGB  	 0.640 0.330 / 0.300 0.600 / 0.150	0.060 --
+//1 SMPTE C  0.630 0.340 / 0.310 0.595 / 0.155	0.070 --
+//2 REC709   0.640 0.330 / 0.300 0.600 / 0.150	0.060 --
+//3 BT2020   0.708 0.292 / 0.170 0.797 / 0.131   0.046 --
+//4 SMPTE240 0.630 0.340 / 0.310 0.595 / 0.155   0.070 --
+//5 NTSC1953 0.670 0.330 / 0.210 0.710 / 0.140   0.080 
+//6 EBU      0.640 0.330 / 0.290 0.600 / 0.150   0.060 --
+
+float CHROMA_A_X, CHROMA_A_Y,CHROMA_B_X, CHROMA_B_Y, CHROMA_C_X, CHROMA_C_Y;
+	if (COLOR_MODE == 0.0 || COLOR_MODE == 2.0 || COLOR_MODE == 6.0) 
+	{
+CHROMA_A_X=0.64;
+CHROMA_A_Y=0.33;
+CHROMA_B_X=0.3;
+CHROMA_B_Y=0.6;
+CHROMA_C_X=	0.15;
+CHROMA_C_Y=	0.06;
+	}
+
+else if (COLOR_MODE == 1.0 || COLOR_MODE == 4.0) 
+	{
+CHROMA_A_X=0.63;
+CHROMA_A_Y=0.34;
+CHROMA_B_X=0.31;
+CHROMA_B_Y=0.595;
+CHROMA_C_X=	0.155;
+CHROMA_C_Y=	0.07;
+	}
+
+else if (COLOR_MODE == 3.0 ) 
+	{
+CHROMA_A_X=0.708;
+CHROMA_A_Y=0.292;
+CHROMA_B_X=0.17;
+CHROMA_B_Y=0.797;
+CHROMA_C_X=	0.131;
+CHROMA_C_Y=	0.046;
+	}
+else if (COLOR_MODE == 6.0 ) 
+	{
+CHROMA_A_X=0.67;
+CHROMA_A_Y=0.33;
+CHROMA_B_X=0.21;
+CHROMA_B_Y=0.71;
+CHROMA_C_X=	0.14;
+CHROMA_C_Y=	0.08;
+	}
 	mat3 xyYrgb = mat3(CHROMA_A_X, CHROMA_A_Y, Yrgb.r,
 	                   CHROMA_B_X, CHROMA_B_Y, Yrgb.g,
 	                   CHROMA_C_X, CHROMA_C_Y, Yrgb.b);
@@ -171,10 +189,90 @@ vec3 Yrgb_to_RGB(mat3 toRGB, vec3 W, vec3 Yrgb)
 	            dot(W, vec3(RGBrgb[0].b, RGBrgb[1].b, RGBrgb[2].b)));
 }
 
+vec3 luminance()
+{
+//0 SRGB  	 0.299 0.587 0.114
+//1 SMPTE C 	 0.299 0.587 0.114 
+//2 REC709  	 0.212 0.715 0.072 
+//3 BT2020    0.262 0.678 0.059 
+//4 SMPTE240  0.212 0.701 0.086 
+//5 NTSC1953  0.299 0.587 0.114 
+//6 EBU       0.299 0.587 0.114 
+
+ float CHROMA_A_WEIGHT, CHROMA_B_WEIGHT, CHROMA_C_WEIGHT;
+ if (COLOR_MODE == 0.0 || COLOR_MODE == 1.0 || 
+ 	  COLOR_MODE == 5.0 || COLOR_MODE == 6.0)
+{
+	CHROMA_A_WEIGHT = 0.299;
+	CHROMA_B_WEIGHT = 0.587;
+	CHROMA_C_WEIGHT = 0.114;
+}
+
+else if (COLOR_MODE == 2.0 || COLOR_MODE == 4.0)
+{
+	CHROMA_A_WEIGHT = 0.212;
+	CHROMA_B_WEIGHT = 0.715;
+	CHROMA_C_WEIGHT = 0.072;
+}
+
+else if (COLOR_MODE == 3.0 )
+{
+	CHROMA_A_WEIGHT = 0.262;
+	CHROMA_B_WEIGHT = 0.678;
+	CHROMA_C_WEIGHT = 0.059;
+}
+
+
+	return vec3(CHROMA_A_WEIGHT, CHROMA_B_WEIGHT, CHROMA_C_WEIGHT);
+}
+
 
 float sdr_linear(const float x)
 {
-	return x < CRT_TR0 ? x / CRT_TR2 : pow((x + CRT_TR) / (1.0+ CRT_TR), GAMMAIN);
+//			  RX   RY      GX     GY     BX      BY      RL    GL    BL       TR1    TR2   TR3
+//0 SRGB  	 0.040 0.055 12.92
+//1 SMPTE C  0.018 0.099 4.5
+//2 REC709   0.018 0.099 4.5
+//3 BT2020   0.059 0.099 4.5
+//4 SMPTE240 0.091 0.111 4.0
+//5 NTSC1953 0.081 0.099 4.5
+//6 EBU      0.081 0.099 4.5
+
+float CRT_TR1 ,CRT_TR2, CRT_TR3;
+
+if (COLOR_MODE == 0.0)
+{
+	CRT_TR1 = 0.040;
+	CRT_TR2 = 0.055;
+	CRT_TR3 = 12.92;
+}
+
+else if (COLOR_MODE == 1.0 || COLOR_MODE == 2.0)
+{
+	CRT_TR1 = 0.018;
+	CRT_TR2 = 0.099;
+	CRT_TR3 = 4.5;
+}
+else if (COLOR_MODE == 3.0 )
+{
+	CRT_TR1 = 0.059;
+	CRT_TR2 = 0.099;
+	CRT_TR3 = 4.5;
+}
+else if (COLOR_MODE == 4.0 )
+{
+	CRT_TR1 = 0.061;
+	CRT_TR2 = 0.111;
+	CRT_TR3 = 4.0;
+}
+else if (COLOR_MODE == 5.0 || COLOR_MODE == 6.0)
+{
+	CRT_TR1 = 0.081;
+	CRT_TR2 = 0.099;
+	CRT_TR3 = 4.5;
+}
+
+	return x < CRT_TR2 ? x / CRT_TR3 : pow((x + CRT_TR1) / (1.0+ CRT_TR1), GAMMAIN);
 }
 
 vec3 sdr_linear(const vec3 x)
@@ -197,7 +295,7 @@ void main()
 	mat3 toRGB = colorspace_rgb();
 	vec3 Yrgb = texture2D(Source, vTexCoord).rgb;
 	Yrgb = sdr_linear(Yrgb);
-	vec3 W = vec3(CHROMA_A_WEIGHT, CHROMA_B_WEIGHT, CHROMA_C_WEIGHT);
+	vec3 W = luminance();
 	vec3 RGB = Yrgb_to_RGB(toRGB, W, Yrgb);
 	if (SCALE_W > 0.0) {
 		vec3 white = Yrgb_to_RGB(toRGB, W, WHITE);
