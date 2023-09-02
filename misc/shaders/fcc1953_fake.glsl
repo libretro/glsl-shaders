59210ff60b2c838ca191edd4a4cfeeb7c6c9c036
metallic77 <43163462+metallic77@users.noreply.github.com>
2023-09-02 20:15:26 +0300

    Update fcc1953_fake.glsl

--- misc/shaders/fcc1953_fake.glsl
+++ misc/shaders/fcc1953_fake.glsl
@@ -3,9 +3,8 @@
 
 */
 
-#pragma parameter bogus " [ PAL: 0.23  0.52  0.10 ] " 0.0 0.0 0.0 0.0
-#pragma parameter R "Red Channel" 0.21 0.0 1.0 0.01
-#pragma parameter G "Green Channel" 0.71 0.0 1.0 0.01
+#pragma parameter R "Red Channel" 0.25 0.0 1.0 0.01
+#pragma parameter G "Green Channel" 0.66 0.0 1.0 0.01
 #pragma parameter B "Blue Channel" 0.09 0.0 1.0 0.01
 
 #define pi 3.14159
