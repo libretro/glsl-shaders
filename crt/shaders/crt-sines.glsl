ec09d150c3fcac7817f919bbfc28ca9f59cceb56
metallic77 <43163462+metallic77@users.noreply.github.com>
2023-09-02 22:40:07 +0300

    Update crt-sines.glsl

--- crt/shaders/crt-sines.glsl
+++ crt/shaders/crt-sines.glsl
@@ -3,6 +3,7 @@
 /* 
   work by DariusG 2023, some ideas borrowed from Dogway's zfast_crt_geo
 
+  v1.4 removed junk, optimized white point a bit
   v1.3 added more color options and Color Temp switch
   v1.2b: Lanczos4 --> Lanczos2 for performance. mask 0 improved
   v1.2: improved mask/scanlines
@@ -12,16 +13,15 @@
 #pragma parameter SHARPNESS "LANCZOS SHARPNESS" 1.66 0.7 2.4 0.01 
 #pragma parameter curv "Curvature"  1.0 0.0 1.0 1.0
 #pragma parameter ssize "Scanline Size" 1.0 1.0 2.0 1.0
-#pragma parameter scanB "Scanline Strength High" 0.6 0.0 1.0 0.05
+#pragma parameter scanB "Scanline Strength High" 0.5 0.0 1.0 0.05
 #pragma parameter scanL "Scanline Strength Low" 0.8 0.0 1.0 0.05
 #pragma parameter Shadowmask "  Mask Type " 0.0 -1.0 2.0 1.0
 #pragma parameter slotx "  Slot Size x" 3.0 2.0 3.0 1.0
 #pragma parameter width "  Mask Width 3.0/2.0 " 0.6666 0.6666 1.0 0.3333
 #pragma parameter mask "  Mask Strength" 0.4 0.0 1.0 0.05
 #pragma parameter sat "Saturation" 1.0 0.0 2.0 0.01
-#pragma parameter colors "Colors: 0.0 BestRGB, 1.0 P22D93, 2.0:NTSC" 0.0 -1.0 2.0 1.0
+#pragma parameter colors "Colors: 0.0 SMPTE-C, 1.0 Sony P22, 2.0:NTSC" 1.0 -1.0 2.0 1.0
 #pragma parameter wp "White Point Adjust" 0.0 -0.25 0.25 0.01
-#pragma parameter Dx "Color Temp: D50, D55, D65, D75" 3.0 -1.0 3.0 1.0
 
 #if defined(VERTEX)
 
@@ -128,7 +128,6 @@ uniform COMPAT_PRECISION float thresh;
 uniform COMPAT_PRECISION float colors;
 uniform COMPAT_PRECISION float sat;
 uniform COMPAT_PRECISION float wp;
-uniform COMPAT_PRECISION float Dx;
 
 #else
 #define width 1.0
@@ -143,7 +142,6 @@ uniform COMPAT_PRECISION float Dx;
 #define colors 0.0
 #define sat 1.0
 #define wp 0.0
-#define Dx -1.0
 #define SHARPNESS 1.66
 
 #endif
@@ -203,20 +201,20 @@ float Mask (vec2 pos, float l)
 }
 
 
-const mat3 P22D93 = mat3(
-     1.00000, 0.00000, -0.06173,
-     0.07111, 0.96887, -0.01136,
-     0.00000, 0.08197,  1.07280);
+const mat3 SonyP22 = mat3(
+1.10544112,  -0.09467967, -0.00547813,
+0.12322469,  0.88445481,  -0.00151429,
+0.02295649,  -0.00169611, 0.98164123);
 
 // NTSC to sRGB matrix, used in linear space
 const mat3 NTSC = mat3(1.5073,  -0.3725, -0.0832, 
                     -0.0275, 0.9350,  0.0670,
                      -0.0272, -0.0401, 1.1677);
 
-const mat3 BestRGB = mat3(
-1.85892969033284,    -0.614661175214, -0.17264295009788,
-0.02751513955392,    1.02513517288247,    -0.0471041506497,
-0.03616629710052,    -0.14857432776786,   0.86276952738936
+const mat3 SMPTE_C = mat3(
+0.93202665669,   0.04110980761,   0.02156250279,
+0.01362929932 ,  0.97099824432 ,  0.01473191244,
+0.005551435508 , -0.01431194472 , 1.00829538798
 );
 
 vec2 Warp(vec2 pos)
@@ -226,14 +224,6 @@ vec2 Warp(vec2 pos)
     return pos*0.5 + 0.5;
 }
 
-vec3 TEMP ()
-{
-    if (Dx == 0.0) return vec3(0.964,1.0,0.8252);
-    else if (Dx == 1.0) return vec3(0.95682,1.0,0.92149);
-    else if (Dx == 2.0) return vec3(0.95047,1.0,1.0888);
-    else if (Dx == 3.0) return vec3(0.94972,1.0,1.22638);
-    else return vec3(1.0);
-}
 
 void main()
 {
@@ -266,32 +256,33 @@ void main()
                
                               
        
-    //pos.y *= 0.995 ;
+   
     float OGL2Pos = fract((pos.y*SourceSize.y)/ssize);
     float lum = max(max(res.r,res.g),res.b);
     float scan = mix (scanL, scanB, lum);
     
-    res.rgb *= res.rgb;
     
     if (colors == 2.0) res.rgb *= NTSC;  else 
-    if (colors == 1.0) res.rgb *= P22D93; else
-    if (colors == 0.0) res.rgb *= BestRGB; else
+    if (colors == 1.0) res.rgb *= SonyP22; else
+    if (colors == 0.0) res.rgb *= SMPTE_C; else
     res.rgb; res = clamp(res, 0.0,1.0);
     
-    float scanline = scan*sin(OGL2Pos*PI*2.0)+1.0-scan+scan*sin(1.0-OGL2Pos*PI*2.0)+1.0-scan;
+    float scanline = scan*sin(OGL2Pos*PI*2.0)+1.0-scan + scan*sin(1.0-OGL2Pos*PI*2.0)+1.0-scan;
     res *= scanline;
     res *= Mask(vTexCoord, lum);
-    res.rgb = sqrt(res.rgb)*TEMP();
-
 
     //CHEAP TEMPERATURE CONTROL     
-    if (wp != 0.0) { res.rgb *= vec3(1.0+wp,1.0,1.0-wp);}
+    if (wp != 0.0) { res.rgb *= vec3(1.0 + 0.08*wp,1.0,1.0-0.8*wp);
+                   if(wp > 0.0)  res.rgb += vec3(0.15*wp,0.0,0.0);
+                   if(wp < 0.0)  res.rgb += vec3(0.0,0.0,0.15*wp);
+                     res = clamp(res, 0.0,1.0);
+    
+    }
     
     //SATURATION
     vec3 lumweight = vec3(0.29,0.6,0.11);
     vec3 grays = vec3(dot(lumweight, res.rgb));
     res.rgb = mix(grays, res.rgb, sat);
-    res *= mix(1.35,1.1, lum);
     // CORNERS
     vec2 c = warpos;
     corn   = min(c, 1.0-c);    // This is used to mask the rounded
