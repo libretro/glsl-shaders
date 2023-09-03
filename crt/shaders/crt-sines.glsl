e32564889a25259d971f9c7f2f68aa56d5f18600
metallic77 <43163462+metallic77@users.noreply.github.com>
2023-09-03 16:21:52 +0300

    Update crt-sines.glsl (#285)
    * Update crt-sines.glsl
    
    * Update crt-sines.glsl
    
    * Update crt-sines.glslp

--- crt/shaders/crt-sines.glsl
+++ crt/shaders/crt-sines.glsl
@@ -2,7 +2,8 @@
 
 /* 
   work by DariusG 2023, some ideas borrowed from Dogway's zfast_crt_geo
-
+  
+  v1.4b added some system specific tweaks
   v1.4 removed junk, optimized white point a bit
   v1.3 added more color options and Color Temp switch
   v1.2b: Lanczos4 --> Lanczos2 for performance. mask 0 improved
@@ -12,8 +13,8 @@
 
 #pragma parameter SHARPNESS "LANCZOS SHARPNESS" 1.66 0.7 2.4 0.01 
 #pragma parameter curv "Curvature"  1.0 0.0 1.0 1.0
-#pragma parameter ssize "Scanline Size" 1.0 1.0 2.0 1.0
 #pragma parameter scanL "Scanline Weight" 0.8 0.0 1.0 0.05
+#pragma parameter interlacing "Interlacing On/Off" 1.0 0.0 1.0 1.0
 #pragma parameter Shadowmask "  Mask Type " 0.0 -1.0 2.0 1.0
 #pragma parameter slotx "  Slot Size x" 3.0 2.0 3.0 1.0
 #pragma parameter width "  Mask Width 3.0/2.0 " 0.6666 0.6666 1.0 0.3333
@@ -21,6 +22,10 @@
 #pragma parameter sat "Saturation" 1.0 0.0 2.0 0.01
 #pragma parameter colors "Colors: 0.0 SMPTE-C, 1.0 Sony P22, 2.0:NTSC" 1.0 -1.0 2.0 1.0
 #pragma parameter wp "White Point Adjust" 0.0 -0.25 0.25 0.01
+#pragma parameter bogus " [ System Tweaks ] " 0.0 0.0 0.0 0.0
+#pragma parameter push_r "PAL Push Red" 0.0 0.0 0.15 0.01
+#pragma parameter NTSC_asp "Amiga NTSC Aspect, eg Monkey Island" 0.0 0.0 01.0 1.0
+#pragma parameter sega "Luminance Fix: Sega, Amiga dark ST colors" 0.0 0.0 2.0 1.0
 
 #if defined(VERTEX)
 
@@ -119,27 +124,33 @@ uniform COMPAT_PRECISION float width;
 uniform COMPAT_PRECISION float slotx;
 uniform COMPAT_PRECISION float mask;
 uniform COMPAT_PRECISION float Shadowmask;
-uniform COMPAT_PRECISION float ssize;
 uniform COMPAT_PRECISION float scanL;
 uniform COMPAT_PRECISION float curv;
 uniform COMPAT_PRECISION float thresh;
 uniform COMPAT_PRECISION float colors;
+uniform COMPAT_PRECISION float push_r;
 uniform COMPAT_PRECISION float sat;
 uniform COMPAT_PRECISION float wp;
+uniform COMPAT_PRECISION float NTSC_asp;
+uniform COMPAT_PRECISION float sega;
+uniform COMPAT_PRECISION float interlacing;
 
 #else
 #define width 1.0
 #define slotx 1.0
 #define Shadowmask 0.0
 #define mask 0.5
-#define ssize 1.0
 #define scanL 0.6
 #define curv 1.0
 #define thresh 0.2
 #define colors 0.0
+#define push_r 0.0
 #define sat 1.0
 #define wp 0.0
 #define SHARPNESS 1.66
+#define NTSC_asp 0.0
+#define sega 0.0
+#define interlacing 1.0
 
 #endif
 
@@ -217,7 +228,7 @@ const mat3 SMPTE_C = mat3(
 vec2 Warp(vec2 pos)
 {
     pos  = pos*2.0-1.0;
-    pos *= vec2(1.0 + (pos.y*pos.y)*0.0276, 1.0 + (pos.x*pos.x)*0.0414);
+    pos *= vec2(1.0 + (pos.y*pos.y)/32.0, 1.0 + (pos.x*pos.x)/24.0);
     return pos*0.5 + 0.5;
 }
 
@@ -231,7 +242,8 @@ void main()
         pos /= scaleS;
         } 
     else pos = vTexCoord;
-    
+    if(NTSC_asp == 1.0) { pos.y *= 200.0/240.0; pos.y += 0.004;}
+
 // LANCZOS 2 taps
             vec2 one_pix = SourceSize.zw;
             pos = pos + one_pix*2.5;
@@ -252,19 +264,28 @@ void main()
                  ;
                
                               
-       
-   
+    float ssize = 1.0;   
+    if (InputSize.y > 400.0) ssize = 2.0;
+
     float OGL2Pos = fract((pos.y*SourceSize.y)/ssize);
+
+    if (interlacing == 1.0 && InputSize.y > 400.0)
+    OGL2Pos = mod(float(FrameCount),2.0) < 1.0? 1.0-OGL2Pos : OGL2Pos;
+    
     float lum = 2.0+dot(vec3(0.666), res);
 
     float scan = pow(scanL, lum);
     
-    
+
+// Some color tweaks    
     if (colors == 2.0) res.rgb *= NTSC;  else 
     if (colors == 1.0) res.rgb *= SonyP22; else
     if (colors == 0.0) res.rgb *= SMPTE_C; else
     res.rgb; res = clamp(res, 0.0,1.0);
-    
+
+    if (lum>2.0)  res.rb += vec2(push_r,push_r/3.0);
+    if (sega == 1.0) res *= 1.06;
+    if (sega == 2.0) res *= 2.0;
     res *= res;
 
 // That 0.4 plus and divide, on a raised luminance dependent 'scanline', removes moire, in crt-Geom style.
@@ -286,7 +307,7 @@ void main()
     // CORNERS
     vec2 c = warpos;
     corn   = min(c, 1.0-c);    // This is used to mask the rounded
-    corn.x = 0.0003333/corn.x; // corners later on
+    corn.x = 0.00038/corn.x; // corners later on
 
     if (corn.y <= corn.x && curv == 1.0 || corn.x < 0.0001 && curv ==1.0 )
     res = vec3(0.0);
