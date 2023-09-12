f4e957bfbaabe23732cd3aaba73a35b0d29b7d97
metallic77 <43163462+metallic77@users.noreply.github.com>
2023-09-12 16:13:21 +0300

    update lanczos2 (#294)
    * Update lanczos2.glslp
    
    * Update lanczos2.glsl
    
    * Update and rename crt-Guest.r-mini.glslp to crt-gdv-mini.glslp
    
    * Rename crt-guest-cgwg-curvature.glsl to crt-gdv-mini.glsl
    
    * Update fake-CRT-Geom.glsl
    
    * Update color_profiler.glsl

--- windowed/shaders/lanczos2.glsl
+++ windowed/shaders/lanczos2.glsl
@@ -96,61 +96,44 @@ uniform COMPAT_PRECISION float SCANLINE;
 #endif
 
 
-#define FIX(c) max(abs(c), 0.000001);
-#define PI  3.1415926535897932384626433832795
-
-vec2 weight2(float x)
-        {
-            const float radius = 2.0;
-            vec2 smpl = FIX(PI * vec2(1.0 - x, x));
-
-            // Lanczos2. Note: we normalize below, so no point in multiplying by radius.
-            vec2 ret = sin(smpl) * sin(smpl / radius) / (smpl * smpl);
-
-            // Normalize
-            return ret / dot(ret, vec2(1.0));
-        }
-
-vec3 pixel(float xpos, float ypos)
-        {
-
-            return COMPAT_TEXTURE(Source, vec2(xpos, ypos)).rgb;
-        }
-
-vec3 line(float ypos, vec2 xpos, vec2 linetaps)
-        {
-            return (pixel(xpos.x, ypos) * linetaps.x +
-                    pixel(xpos.y, ypos) * linetaps.y) ;
-        }
-
-
-
-
 void main()
-        {
-            
-// LANCZOS 2 taps
-            vec2 one_pix = SourceSize.zw;
-            vec2 pos = vTexCoord + one_pix*2.5;
-            vec2 f = fract(pos * SourceSize.xy);
+{
+  
+    // Source position in fractions of a texel
+    vec2 src_pos = vTexCoord*SourceSize.xy;
+    // Source bottom left texel centre
+    vec2 src_centre = floor(src_pos - 0.5) + 0.5;
+    // f is position. f.x runs left to right, y bottom to top, z right to left, w top to bottom
+    vec4 f; 
+    f.xy = src_pos - src_centre;
+    f.zw = 1.0 - f.xy;
+    // Calculate weights in x and y in parallel.
+    // These polynomials are piecewise approximation of Lanczos kernel
+    // Calculator here: https://gist.github.com/going-digital/752271db735a07da7617079482394543
+    vec4 l2_w0_o3 = (( 1.5672 * f - 2.6445) * f + 0.0837) * f + 0.9976;
+    vec4 l2_w1_o3 = ((-0.7389 * f + 1.3652) * f - 0.6295) * f - 0.0004;
 
-            vec2 linetaps   = weight2(f.x);
-            vec2 columntaps = weight2(f.y);
+    vec4 w1_2  = l2_w0_o3;
+    vec2 w12   = w1_2.xy + w1_2.zw;
+    vec4 wedge = l2_w1_o3 * vec4 (w12.yx, w12.yx);
 
-            vec2 xystart = pos - one_pix*(f + 1.5);
-            vec2 xpos = vec2(
-                xystart.x,
-                xystart.x - one_pix.x 
-              );
+    // Calculate texture read positions. tc12 uses bilinear interpolation to do 4 reads in 1.
+    vec2 tc12 = SourceSize.zw * (src_centre + w1_2.zw / w12);
+    vec2 tc0  = SourceSize.zw * (src_centre - 1.0);
+    vec2 tc3  = SourceSize.zw * (src_centre + 2.0);
+    
+    // Sharpening adjustment
+    float sum = wedge.x + wedge.y + wedge.z + wedge.w + w12.x * w12.y;    
+    wedge /= sum;
 
-        vec3 rgb = 
-                line(xystart.y                  , xpos, linetaps)* columntaps.x +
-                line(xystart.y - one_pix.y      , xpos, linetaps)* columntaps.y
-                 ;
-               
-                rgb = clamp(rgb,0.0,1.0);
-        
+    vec3 col = vec3(
+        COMPAT_TEXTURE(Source, vec2(tc12.x, tc0.y)).rgb * wedge.y +
+        COMPAT_TEXTURE(Source, vec2(tc0.x, tc12.y)).rgb * wedge.x +
+        COMPAT_TEXTURE(Source, tc12.xy).rgb * (w12.x * w12.y) +
+        COMPAT_TEXTURE(Source, vec2(tc3.x, tc12.y)).rgb * wedge.z +
+        COMPAT_TEXTURE(Source, vec2(tc12.x, tc3.y)).rgb * wedge.w
+    );
 
-        FragColor.rgb = rgb;
-        }
+    FragColor = vec4(col,1.0);
+}
 #endif
