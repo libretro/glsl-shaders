bed7e61a2d922e4dbbd53ba3c3c1240b6355c2d5
metallic77 <43163462+metallic77@users.noreply.github.com>
2023-10-23 16:06:37 +0300

    update zfast-composite (#323)
    * Update zfast_crt_composite.glsl
    
    * Update zfast_crt_composite.glsl
    
    * Update zfast_crt_composite.glsl

--- crt/shaders/zfast_crt_composite.glsl
+++ crt/shaders/zfast_crt_composite.glsl
@@ -21,15 +21,15 @@
 
 // Parameter lines go here:
 #pragma parameter Curvature "Curvature" 1.0 0.0 1.0 1.0
-#pragma parameter blurx "Convergence X-Axis" 0.85 -2.0 2.0 0.05
+#pragma parameter blurx "Convergence X-Axis" 0.6 -2.0 2.0 0.05
 #pragma parameter blury "Convergence Y-Axis" -0.10 -2.0 2.0 0.05
-#pragma parameter HIGHSCANAMOUNT1 "Scanline Amount (Dark)" 0.40 0.0 1.0 0.05
-#pragma parameter HIGHSCANAMOUNT2 "Scanline Amount (Bright)" 0.25 0.0 1.0 0.05
+#pragma parameter HIGHSCANAMOUNT1 "Scanline Amount (Dark)" 0.3 0.0 1.0 0.05
+#pragma parameter HIGHSCANAMOUNT2 "Scanline Amount (Bright)" 0.2 0.0 1.0 0.05
 #pragma parameter TYPE "Mask Type" 0.0 0.0 1.0 1.0
 #pragma parameter MASK_DARK "Mask Effect Amount" 0.3 0.0 1.0 0.05
 #pragma parameter MASK_FADE "Mask/Scanline Fade" 0.7 0.0 1.0 0.05
 #pragma parameter sat "Saturation" 1.0 0.0 2.0 0.05
-#pragma parameter ntsc "NTSC Colors" 0.0 0.0 1.0 1.0
+#pragma parameter ntsc "Colors: NTSC-U, NTSC-J" 0.0 0.0 2.0 1.0
 
 #define pi 3.14159
 #define scale vec4(TextureSize/InputSize,InputSize/TextureSize)
@@ -97,8 +97,6 @@ uniform COMPAT_PRECISION vec2 InputSize;
 
 // compatibility #defines
 #define vTexCoord TEX0.xy
-#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
-#define OutSize vec4(OutputSize, 1.0 / OutputSize)
 
 void main()
 {
@@ -162,9 +160,18 @@ vec2 Warp(vec2 pos)
 }
 
 // NTSC to sRGB matrix, used in linear space
-const mat3 NTSC = mat3(1.5073,  -0.3725, -0.0832, 
-                    -0.0275, 0.9350,  0.0670,
-                     -0.0272, -0.0401, 1.1677);
+mat3 NTSC = mat3(                   
+0.9462  ,   0.0228  ,   0.0422  ,
+0.0286  ,   0.9471  ,   0.0236  ,
+0.0134  ,   -0.0389 ,   1.1184  );
+
+
+// standard 8500k
+mat3 NTSC_J = mat3(                 
+1.0594  ,   -0.1021 ,   0.0595  ,
+0.0956  ,   0.8771  ,   0.0295  ,
+0.0070  ,   -0.0190 ,   1.2647  );
+
 
 
 void main()
@@ -173,57 +180,64 @@ void main()
     if (Curvature == 1.0) 
 {
     pos = Warp(TEX0.xy*(scale.xy));
-    corn = min(pos,vec2(1.0)-pos); // This is used to mask the rounded
-    corn.x = 0.00001/corn.x;           // corners later on
+
+// corners calculation
+    corn = min(pos,vec2(1.0)-pos);      
+    corn.x = 0.000015/corn.x;           
+    
     pos *= scale.zw;
 }
 
     else pos = vTexCoord;
-	
-     float OGL2Pos = pos.y*TextureSize.y;
-     float cent = floor(OGL2Pos)+0.5;
-     float ycoord = cent*SourceSize.w; 
-     ycoord = mix(pos.y,ycoord,0.6);
-     pos = vec2(pos.x,ycoord);
-	 
-	
 
-	 vec3 sample1 =  COMPAT_TEXTURE(Source,vec2(pos.x + blur_x, pos.y - blur_y)).rgb;
-	 vec3 sample2 =  0.5*COMPAT_TEXTURE(Source,pos).rgb;
-	 vec3 sample3 =  COMPAT_TEXTURE(Source,vec2(pos.x - blur_x, pos.y + blur_y)).rgb;
+// hermite-like filter (blurrier), fast & cleans juggy edges	
+    vec2 OGL2Pos = pos*TextureSize.xy;
+    vec2 p = OGL2Pos + 0.5; float y = p.y;
+    vec2 i = floor( p );
+    vec2 f = fract( p );
+    p = i + f*f;
+    p = (p - 0.5)*SourceSize.zw;
+
+// convergence calculations
+	 vec3 sample1 =  COMPAT_TEXTURE(Source,vec2(p.x + blur_x, p.y - blur_y)).rgb;
+	 vec3 sample2 =  0.5*COMPAT_TEXTURE(Source,p).rgb;
+	 vec3 sample3 =  COMPAT_TEXTURE(Source,vec2(p.x - blur_x, p.y + blur_y)).rgb;
 	
 	 vec3 colour = vec3 (sample1.r*0.5  + sample2.r, 
 		                 sample1.g*0.25 + sample2.g + sample3.g*0.25, 
 		                                  sample2.b + sample3.b*0.5);
-     vec3 lumweight=vec3(0.22,0.71,0.07);
-     float lumsat = dot(colour,lumweight);
-   
-     vec3 graycolour = vec3(lumsat);
-     colour = vec3(mix(graycolour,colour.rgb,sat));
+//color conversions
+  if (ntsc != 0.0)
+{
+    if (ntsc == 1.0) colour *= NTSC; 
+    if (ntsc == 2.0) colour *= NTSC_J; 
+    colour = clamp(colour,0.0,1.0);}
 
      float SCANAMOUNT = mix(HIGHSCANAMOUNT1,HIGHSCANAMOUNT2,max(max(colour.r,colour.g),colour.b));
 	    
-    colour.rgb *= colour.rgb;
-	 
+    colour *= colour;
+
+// scanlines	 
 	if (InputSize.y > 400.0) {
     colour ;
 	} 
 else {
-	colour *= SCANAMOUNT * sin(fract(OGL2Pos)*3.14159)+1.0-SCANAMOUNT;
-	colour *= SCANAMOUNT * sin(fract(1.0-OGL2Pos)*3.14159)+1.0-SCANAMOUNT;
+	colour *= SCANAMOUNT * sin((y*2.0)*3.14159)+1.0-SCANAMOUNT;
 	}
 
+//mask
 	 float steps; if (TYPE == 0.0) steps = 0.5; else steps = 0.3333;
 	 float whichmask = fract(gl_FragCoord.x*steps);
 	 float mask = 1.0 + float(whichmask < steps) * -MASK_DARK;
 
-
-	colour.rgb = mix(mask*colour, colour, dot(colour.rgb,vec3(maskFade)));
+	colour = mix(mask*colour, colour, dot(colour,vec3(maskFade)));
     
-    if (ntsc == 1.0) {colour.rgb *= NTSC; colour.rgb = clamp(colour.rgb,0.0,1.0);}
-    
-    colour.rgb = sqrt(colour.rgb);
+    colour = sqrt(colour);
 
+     float lumsat = dot(colour,vec3(0.3,0.6,0.1));
+     colour = vec3(mix(vec3(lumsat),colour,sat));
+
+// apply corners
     if (Curvature == 1.0 && corn.y < corn.x || Curvature == 1.0 && corn.x < 0.00001 )
     colour = vec3(0.0); 
     
