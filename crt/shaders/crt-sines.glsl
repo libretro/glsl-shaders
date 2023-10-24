0cc2993cbf3abba6f22ab811e2dadccd8782c754
metallic77 <43163462+metallic77@users.noreply.github.com>
2023-10-24 08:42:10 +0300

    Update crt-sines.glsl

--- crt/shaders/crt-sines.glsl
+++ crt/shaders/crt-sines.glsl
@@ -21,27 +21,23 @@
 
 #pragma parameter bogus_ms " [ SCANLINES/MASK ] " 0.0 0.0 1.0 0.0
 #pragma parameter interlacing "Interlacing On/Off" 1.0 0.0 1.0 1.0
-
-#pragma parameter SCANLOW "Scanline Low" 0.5 0.0 1.0 0.05
-#pragma parameter SCANHIGH "Scanline High" 0.25 0.0 1.0 0.05
-
+#pragma parameter SCANLOW "Scanline Low" 0.6 0.0 1.0 0.05
+#pragma parameter SCANHIGH "Scanline High" 0.3 0.0 1.0 0.05
 #pragma parameter mask "Mask type: 1-Dotmask, 2-RGB, 3-SlotMask" 2.0 0.0 3.0 1.0
-#pragma parameter m_str "Mask Strength" 0.25 0.0 0.5 0.05
+#pragma parameter m_str "Mask Strength" 0.3 0.0 0.5 0.05
 #pragma parameter slotx "Slot Mask Size x" 3.0 2.0 3.0 1.0
-#pragma parameter width "Slot Mask Width 3.0/2.0 " 0.6666 0.6666 1.0 0.3333
+
 #pragma parameter bogus_conv " [ CONVERGENCE ] " 0.0 0.0 1.0 0.0
 #pragma parameter RX "Red Convergence Horiz." 0.0 -1.0 1.0 0.05
 #pragma parameter RY "Red Convergence Vert." 0.0 -1.0 1.0 0.05
-
 #pragma parameter GX "Green Convergence Horiz." 0.0 -1.0 1.0 0.05
 #pragma parameter GY "Green Convergence Vert." 0.0 -1.0 1.0 0.05
-
 #pragma parameter BX "Blue Convergence Horiz." 0.0 -1.0 1.0 0.05
 #pragma parameter BY "Blue Convergence Vert." 0.0 -1.0 1.0 0.05
-#pragma parameter bogus_col " [ COLORS ] " 0.0 0.0 1.0 0.0
 
-#pragma parameter BOOST "Bright Boost" 0.15 0.0 1.0 0.01
-#pragma parameter SAT "Saturation" 1.0 0.0 2.0 0.01
+#pragma parameter bogus_col " [ COLORS ] " 0.0 0.0 1.0 0.0
+#pragma parameter BOOST "Bright Boost" 0.15 0.0 1.0 0.05
+#pragma parameter SAT "Saturation" 1.0 0.0 2.0 0.05
 #pragma parameter CRT "Trinitron Colors, 1:PC1, 2:PC2, 3:Android" 1.0 0.0 3.0 1.0
 
 
@@ -74,6 +70,8 @@ COMPAT_VARYING vec2 scale;
 COMPAT_VARYING vec2 maskpos;
 COMPAT_VARYING vec2 warpp;
 COMPAT_VARYING vec2 ps;
+COMPAT_VARYING vec3 dx;
+COMPAT_VARYING vec3 dy;
 
 
 vec4 _oPosition1; 
@@ -90,10 +88,20 @@ uniform COMPAT_PRECISION vec2 InputSize;
 
 #ifdef PARAMETER_UNIFORM
 uniform COMPAT_PRECISION float mask;
-
+uniform COMPAT_PRECISION float RX;
+uniform COMPAT_PRECISION float RY;
+uniform COMPAT_PRECISION float GX;
+uniform COMPAT_PRECISION float GY;
+uniform COMPAT_PRECISION float BX;
+uniform COMPAT_PRECISION float BY;
 #else
 #define mask     1.0      
-   
+#define RX 0.0     
+#define RY 0.0     
+#define GX 0.0     
+#define GY 0.0     
+#define BX 0.0     
+#define BY 0.0     
 #endif
 
 void main()
@@ -103,10 +111,11 @@ void main()
     scale = TextureSize.xy/InputSize.xy;
     float size = 1.0;
     if (mask == 2.0) size = 0.6667;
-
     maskpos = TEX0.xy*OutputSize.xy*TextureSize.xy/InputSize.xy*size;
     warpp = TEX0.xy*scale*2.0-1.0;
     ps = 1.0/TextureSize.xy;
+    dx = ps.x*vec3(RX,GX,BX);
+    dy = ps.y*vec3(RY,GY,BY);
 }
 
 #elif defined(FRAGMENT)
@@ -143,7 +152,8 @@ COMPAT_VARYING vec2 scale;
 COMPAT_VARYING vec2 maskpos;
 COMPAT_VARYING vec2 warpp;
 COMPAT_VARYING vec2 ps;
-
+COMPAT_VARYING vec3 dx;
+COMPAT_VARYING vec3 dy;
 
 // compatibility #defines
 #define Source Texture
@@ -158,35 +168,21 @@ uniform COMPAT_PRECISION float SCANHIGH;
 uniform COMPAT_PRECISION float SHARPX;
 uniform COMPAT_PRECISION float mask;
 uniform COMPAT_PRECISION float m_str;
-uniform COMPAT_PRECISION float RX;
-uniform COMPAT_PRECISION float RY;
-uniform COMPAT_PRECISION float GX;
-uniform COMPAT_PRECISION float GY;
-uniform COMPAT_PRECISION float BX;
-uniform COMPAT_PRECISION float BY;
 uniform COMPAT_PRECISION float BOOST;
 uniform COMPAT_PRECISION float SAT;
 uniform COMPAT_PRECISION float CRT;
 uniform COMPAT_PRECISION float CURV;
 uniform COMPAT_PRECISION float NTSC_asp;
 uniform COMPAT_PRECISION float slotx;
-uniform COMPAT_PRECISION float width;
 uniform COMPAT_PRECISION float interlacing;
 
 #else
-#define width 1.0
 #define slotx 1.0
 #define mask 1.0
 #define SCANLOW 0.5
 #define SCANHIGH 0.3
 #define SHARPX 0.3
 #define m_str 0.3          
-#define RX 0.0     
-#define RY 0.0     
-#define GX 0.0     
-#define GY 0.0     
-#define BX 0.0     
-#define BY 0.0     
 #define BOOST 0.0     
 #define SAT 1.3     
 #define CRT 1.0     
@@ -206,8 +202,8 @@ if (mask == 3.0)
     {
         oddx = mod(maskpos.x,slotx*2.0) < slotx ? 1.0 : 0.0;
 
-        return        m_str*((sin(pos.x*width*pi)) 
-                    + (sin((pos.y+oddx)*pi)))+1.0-m_str ;
+        return m_str*(sin(pos.x*2.0/slotx*pi) 
+              + sin((pos.y+oddx)*pi))+1.0-m_str ;
     }
 
 if (mask == 0.0) return 1.0;
@@ -221,9 +217,9 @@ vec2 Warp(vec2 pos)
 }
 
 mat3 huePC = mat3(
-    1.05,  -0.05, 0.1,
+    1.05, -0.05, 0.1,
     -0.1, 1.2, -0.2,
-    0.15, 0.15, 1.0
+    0.1, 0.15, 1.0
 );
 mat3 huePC2 = mat3(
     1.0,  0.0, 0.04,
@@ -236,7 +232,6 @@ mat3 hueAnd = mat3(
     0.04, 0.11, 1.05
 );
 
-
 void main()
 {
   vec2 pos,c,corn;
@@ -245,7 +240,7 @@ void main()
      // CORNERS
     c = pos;
     corn   = min(c, 1.0-c);    // This is used to mask the rounded
-    corn.x = 0.00015/corn.x; // corners later on
+    corn.x = 0.0001/corn.x; // corners later on
   pos /= scale;
   }
   else pos = vTexCoord;  
@@ -255,9 +250,6 @@ void main()
 
   vec2 bpos = pos;
 
-  vec2 dx = vec2(ps.x,0.0);
-  vec2 dy = vec2(0.0,ps.y);
-  
   vec2 p = pos*SourceSize.xy ;
   vec2 i = floor(pos*SourceSize.xy) + 0.5;
   vec2 f = p - i;        // -0.5 to 0.5
@@ -266,10 +258,11 @@ void main()
 
     p.x = mix(p.x,pos.x,SHARPX);
 
+
   vec3 res = COMPAT_TEXTURE(Source,p).rgb;
-  float r =  COMPAT_TEXTURE(Source,p + dx*RX + dy*RY).r;
-  float g =  COMPAT_TEXTURE(Source,p + dx*GX + dy*GY).g;
-  float b =  COMPAT_TEXTURE(Source,p + dx*BX + dy*BY).b;
+  float r =  COMPAT_TEXTURE(Source,p + vec2(dx.r,dy.r)).r;
+  float g =  COMPAT_TEXTURE(Source,p + vec2(dx.g,dy.g)).g;
+  float b =  COMPAT_TEXTURE(Source,p + vec2(dx.b,dy.b)).b;
 
   vec3 conv = vec3(r,g,b);
 
@@ -278,6 +271,7 @@ void main()
   if(CRT == 1.0) res *= huePC;
   if(CRT == 2.0) res *= huePC2;
   if(CRT == 3.0) res *= hueAnd;
+  
   vec3 clean = res;
   float w = dot(vec3(BOOST),clean);
   float l = max(max(res.r,res.g),res.b);
@@ -296,7 +290,6 @@ void main()
 
   res *= sqrt(scn*Mask());
 
-  
   vec3 lumweight = vec3(0.29,0.6,0.11);
 
   float grays = dot(lumweight,res);
