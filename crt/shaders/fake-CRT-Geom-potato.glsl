a375d400dde9abc5cd723f53423c7d30b30d7165
metallic77 <43163462+metallic77@users.noreply.github.com>
2023-11-16 11:53:47 +0200

    Update fake-CRT-Geom-potato.glsl

--- crt/shaders/fake-CRT-Geom-potato.glsl
+++ crt/shaders/fake-CRT-Geom-potato.glsl
@@ -1,10 +1,8 @@
-// Simple scanlines and mask effect by DariusG
-// written on an old netbook with 9 gflops
-// and runs 60-65 fps on 720p
+#version 110
 
-#pragma parameter SEVTWO "Scanline Size" 2.0 1.0 2.0 1.0
 
-#define pi 3.1415926
+
+#define pi 3.1415926535897932384626433
 
 #if defined(VERTEX)
 
@@ -29,8 +27,11 @@ COMPAT_ATTRIBUTE vec4 COLOR;
 COMPAT_ATTRIBUTE vec4 TexCoord;
 COMPAT_VARYING vec4 COL0;
 COMPAT_VARYING vec4 TEX0;
+COMPAT_VARYING vec2 scale;
 COMPAT_VARYING float fragpos;
-COMPAT_VARYING float scanpos;
+COMPAT_VARYING vec2 warpp;
+COMPAT_VARYING vec2 dbwarp;
+
 
 vec4 _oPosition1; 
 uniform mat4 MVPMatrix;
@@ -39,24 +40,26 @@ uniform COMPAT_PRECISION int FrameCount;
 uniform COMPAT_PRECISION vec2 OutputSize;
 uniform COMPAT_PRECISION vec2 TextureSize;
 uniform COMPAT_PRECISION vec2 InputSize;
-uniform COMPAT_PRECISION float SEVTWO;
 
 // compatibility #defines
 #define vTexCoord TEX0.xy
 
 #ifdef PARAMETER_UNIFORM
-uniform COMPAT_PRECISION float WHATEVER;
+uniform COMPAT_PRECISION float SIZE;
+
 #else
-#define WHATEVER 0.0
+#define SIZE     1.0      
+   
 #endif
 
 void main()
 {
     gl_Position = MVPMatrix * VertexCoord;
-    TEX0.xy = TexCoord.xy*1.0001;
-    fragpos = TEX0.x*OutputSize.x*TextureSize.x/InputSize.x*pi;
-	float y = TEX0.y*TextureSize.y-0.25;
-	scanpos = y*pi*SEVTWO;
+    TEX0.xy = TexCoord.xy;
+    scale = TextureSize.xy/InputSize.xy;
+    warpp = TEX0.xy*scale;
+    dbwarp = warpp*2.0-1.0;
+    fragpos = warpp.x*OutputSize.x*pi*0.6666;
 }
 
 #elif defined(FRAGMENT)
@@ -89,8 +92,11 @@ uniform COMPAT_PRECISION vec2 TextureSize;
 uniform COMPAT_PRECISION vec2 InputSize;
 uniform sampler2D Texture;
 COMPAT_VARYING vec4 TEX0;
+COMPAT_VARYING vec2 scale;
 COMPAT_VARYING float fragpos;
-COMPAT_VARYING float scanpos;
+COMPAT_VARYING vec2 warpp;
+COMPAT_VARYING vec2 dbwarp;
+
 
 // compatibility #defines
 #define Source Texture
@@ -100,26 +106,58 @@ COMPAT_VARYING float scanpos;
 #define OutSize vec4(OutputSize, 1.0 / OutputSize)
 
 #ifdef PARAMETER_UNIFORM
-uniform COMPAT_PRECISION float SCANLINE_BASE_BRIGHTNESS;
+uniform COMPAT_PRECISION float SIZE;
 
 #else
-#define SCANLINE_BASE_BRIGHTNESS 0.30
-
+#define SIZE  1.0      
+    
 #endif
+
+vec2 Warp(vec2 pos)
+{
+    pos = dbwarp;
+    pos *= vec2(1.0+pos.y*pos.y*0.03, 1.0+pos.x*pos.x*0.04);
+    pos = pos*0.5+0.5;
+    return pos;
+}
 
 void main()
 {
-	float y = TEX0.y*SourceSize.y;
-	float centr = (floor(y)+0.5)/SourceSize.y;
-    float ycoord = mix(centr, vTexCoord.y,0.2); 
-    vec3 res = COMPAT_TEXTURE(Source, vec2(vTexCoord.x, ycoord)).rgb;
-	vec3 origin = res;
-	float lum = dot(vec3(0.2), res);
-	
-     res *= 0.4*sin(scanpos)+0.6 ; 
-     res *= 0.15*sin(fragpos)+0.85;
-	 res = mix(res, origin, lum);
-	 res *= mix(1.35,1.0,lum);
-    FragColor = vec4(res,1.0);
-} 
+vec2 pos = Warp(warpp);
+vec2 corn = min(pos, 1.0-pos);    // This is used to mask the rounded
+  corn.x = 0.0002/corn.x;         // corners later on
+  pos /= scale;
+
+float y = pos.y*SourceSize.y;
+float gl2pos = floor(y) + 0.5;
+float near = gl2pos*SourceSize.w;
+float dy = y - gl2pos;
+      dy = dy*dy*dy*4.0*SourceSize.w;
+   
+vec2 p = vec2(pos.x, near+dy);
+
+vec3 res = COMPAT_TEXTURE(Source,p).rgb;
+
+vec3 clean = res;
+float w = dot(vec3(0.15),res);
+
+// vignette  
+float x = (warpp.x-0.5);  // range -0.5 to 0.5, 0.0 being center of screen
+      x = x*x;   
+res *= (0.25+x)*sin((y-0.25)*pi*2.0)+(0.75-x);
+res *= 0.15*sin(fragpos)+0.85;
+
+res = mix(res,clean, w);
+
+res *= vec3(1.0,0.85,1.2);
+
+
+float lum = dot(vec3(0.29,0.6,0.11),res);
+res = mix(vec3(lum),res, 1.1);
+
+res *= mix(1.25,1.0,w);
+if (corn.y <= corn.x || corn.x < 0.0001 )res = vec3(0.0);
+
+FragColor.rgb = res;   
+}
 #endif
