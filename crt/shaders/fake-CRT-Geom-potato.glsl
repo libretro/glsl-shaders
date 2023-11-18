1833aa041ec64f2ca846d1e3bec49f628e9763b6
metallic77 <43163462+metallic77@users.noreply.github.com>
2023-11-18 19:29:21 +0200

    Update fake-CRT-Geom-potato.glsl

--- crt/shaders/fake-CRT-Geom-potato.glsl
+++ crt/shaders/fake-CRT-Geom-potato.glsl
@@ -2,47 +2,16 @@
 
 #pragma parameter SIZE "Mask Type" 2.0 2.0 3.0 1.0
 
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
+#define OutSize vec4(OutputSize, 1.0 / OutputSize)
 #define pi 3.1415926535897932384626433
-
-#if defined(VERTEX)
-
-#if __VERSION__ >= 130
-#define COMPAT_VARYING out
-#define COMPAT_ATTRIBUTE in
-#define COMPAT_TEXTURE texture
-#else
-#define COMPAT_VARYING varying 
-#define COMPAT_ATTRIBUTE attribute 
-#define COMPAT_TEXTURE texture2D
-#endif
 
 #ifdef GL_ES
 #define COMPAT_PRECISION mediump
+precision mediump float;
 #else
 #define COMPAT_PRECISION
 #endif
-
-COMPAT_ATTRIBUTE vec4 VertexCoord;
-COMPAT_ATTRIBUTE vec4 COLOR;
-COMPAT_ATTRIBUTE vec4 TexCoord;
-COMPAT_VARYING vec4 COL0;
-COMPAT_VARYING vec4 TEX0;
-COMPAT_VARYING vec2 scale;
-COMPAT_VARYING float fragpos;
-COMPAT_VARYING vec2 warpp;
-COMPAT_VARYING vec2 dbwarp;
-
-
-vec4 _oPosition1; 
-uniform mat4 MVPMatrix;
-uniform COMPAT_PRECISION int FrameDirection;
-uniform COMPAT_PRECISION int FrameCount;
-uniform COMPAT_PRECISION vec2 OutputSize;
-uniform COMPAT_PRECISION vec2 TextureSize;
-uniform COMPAT_PRECISION vec2 InputSize;
-
-// compatibility #defines
-#define vTexCoord TEX0.xy
 
 #ifdef PARAMETER_UNIFORM
 uniform COMPAT_PRECISION float SIZE;
@@ -51,67 +20,36 @@ uniform COMPAT_PRECISION float SIZE;
 #define SIZE     1.0      
    
 #endif
+uniform vec2 OutputSize;
+uniform vec2 TextureSize;
+uniform vec2 InputSize;
+varying vec2 TEX0;
+varying vec2 scale;
+varying float fragpos;
+varying vec2 warpp;
+varying vec2 dbwarp;
+
+#if defined(VERTEX)
+uniform mat4 MVPMatrix;
+attribute vec4 VertexCoord;
+attribute vec2 TexCoord;
+
 
 void main()
-{
-    gl_Position = MVPMatrix * VertexCoord;
-    TEX0.xy = TexCoord.xy;
-    scale = TextureSize.xy/InputSize.xy;
-    warpp = TEX0.xy*scale;
-    dbwarp = warpp*2.0-1.0;
-    fragpos = warpp.x*OutputSize.x*pi*2.0/SIZE;
+{	
+	TEX0 = TexCoord*1.0001;
+	gl_Position = MVPMatrix * VertexCoord;
+	scale = TextureSize.xy/InputSize.xy;
+   warpp = TEX0.xy*scale;
+   dbwarp = warpp*2.0-1.0;
+   fragpos = warpp.x*OutputSize.x*pi*2.0/SIZE;
 }
 
 #elif defined(FRAGMENT)
 
-#if __VERSION__ >= 130
-#define COMPAT_VARYING in
-#define COMPAT_TEXTURE texture
-out vec4 FragColor;
-#else
-#define COMPAT_VARYING varying
-#define FragColor gl_FragColor
-#define COMPAT_TEXTURE texture2D
-#endif
-
-#ifdef GL_ES
-#ifdef GL_FRAGMENT_PRECISION_HIGH
-precision highp float;
-#else
-precision mediump float;
-#endif
-#define COMPAT_PRECISION mediump
-#else
-#define COMPAT_PRECISION
-#endif
-
-uniform COMPAT_PRECISION int FrameDirection;
-uniform COMPAT_PRECISION int FrameCount;
-uniform COMPAT_PRECISION vec2 OutputSize;
-uniform COMPAT_PRECISION vec2 TextureSize;
-uniform COMPAT_PRECISION vec2 InputSize;
 uniform sampler2D Texture;
-COMPAT_VARYING vec4 TEX0;
-COMPAT_VARYING vec2 scale;
-COMPAT_VARYING float fragpos;
-COMPAT_VARYING vec2 warpp;
-COMPAT_VARYING vec2 dbwarp;
 
 
-// compatibility #defines
-#define Source Texture
-#define vTexCoord TEX0.xy
-
-#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
-#define OutSize vec4(OutputSize, 1.0 / OutputSize)
-
-#ifdef PARAMETER_UNIFORM
-uniform COMPAT_PRECISION float something;
-
-#else
-#define something  1.0      
-    
-#endif
 
 vec2 Warp(vec2 pos)
 {
@@ -136,20 +74,20 @@ float dy = y - gl2pos;
    
 vec2 p = vec2(pos.x, near+dy);
 
-vec3 res = COMPAT_TEXTURE(Source,p).rgb;
+vec3 res = texture2D(Texture,p).rgb;
 
 vec3 clean = res;
 float w = dot(vec3(0.15),res);
 
 // vignette  
 float x = (warpp.x-0.5);  // range -0.5 to 0.5, 0.0 being center of screen
-      x = x*x;   
+      x = x*x; 
 res *= (0.25+x)*sin((y-0.25)*pi*2.0)+(0.75-x);
 res *= 0.15*sin(fragpos)+0.85;
 
 res = mix(res,clean, w);
 
-res *= vec3(1.0,0.85,1.2);
+res *= vec3(1.0,0.85,1.15);
 
 
 float lum = dot(vec3(0.29,0.6,0.11),res);
@@ -158,6 +96,6 @@ res = mix(vec3(lum),res, 1.1);
 res *= mix(1.25,1.0,w);
 if (corn.y <= corn.x || corn.x < 0.0001 )res = vec3(0.0);
 
-FragColor.rgb = res;   
+gl_FragColor.rgb = res;   
 }
 #endif
