17368e24851cecb8935c74b1a8972fe866d9067d
metallic77 <43163462+metallic77@users.noreply.github.com>
2023-11-20 16:08:23 +0200

    update fake-crt-geom (#355)
    * Update fake-CRT-Geom.glslp
    
    * Update fake-CRT-Geom.glsl
    
    * Update fake-CRT-Geom.glsl

--- crt/shaders/fake-CRT-Geom.glsl
+++ crt/shaders/fake-CRT-Geom.glsl
@@ -1,22 +1,32 @@
-// Simple scanlines with curvature and mask effects lifted from crt-geom
-// original by hunterk, edit by DariusG
+#version 110
 
-///////////////////////  Runtime Parameters  ///////////////////////
-#pragma parameter sharpx "Horizontal Sharpness" 1.5 1.0 5.0 0.05
-#pragma parameter SCANLINE_SINE_COMP_B "Scanline Intensity" 0.25 0.1 0.6 0.05
-#pragma parameter SIZE "Scanline size" 1.0 0.5 1.0 0.5
-#pragma parameter warpX "warpX" 0.03 0.0 0.125 0.01
-#pragma parameter warpY "warpY" 0.05 0.0 0.125 0.01
-#pragma parameter corner_round "Corner Roundness" 0.030 0.005 0.100 0.005
-#pragma parameter MSIZE "Mask Type: Coarse-Fine" 1.0 0.666 1.0 0.3333
-#pragma parameter cgwg "Mask Brightness" 0.7 0.0 1.0 0.1
-#pragma parameter monitor_gamma "Monitor Gamma (Out)" 2.0 1.0 4.0 0.05
-#pragma parameter boost "Bright boost " 0.08 0.00 1.00 0.02
-#pragma parameter GLOW_LINE "Glowing line" 0.006 0.00 0.20 0.001
+#pragma parameter bogus1 " [ COLORS ] " 0.0 0.0 0.0 0.0
+#pragma parameter a_gamma_in "Gamma In" 2.45 1.0 4.0 0.05
+#pragma parameter a_gamma_out "Gamma Out" 2.25 1.0 4.0 0.05
+#pragma parameter a_col_temp "Color Temperature (0.01 ~ 200K)" 0.0 -0.15 0.15 0.01
+#pragma parameter a_sat "Saturation" 1.0 0.0 2.0 0.05
+#pragma parameter a_boostd "Bright Boost Dark" 1.3 0.0 2.0 0.05
+#pragma parameter a_boostb "Bright Boost Bright" 1.05 0.0 2.0 0.05
+#pragma parameter bogus2 " [ SCANLINES/MASK ] " 0.0 0.0 0.0 0.0
+#pragma parameter scanl "Scanlines Low" 0.4 0.0 0.5 0.05
+#pragma parameter scanh "Scanlines High" 0.2 0.0 0.5 0.05
+#pragma parameter a_interlace "Interlace On/Off" 1.0 0.0 1.0 1.0
+#pragma parameter a_MTYPE "Mask Type, Fine/Coarse/LCD" 0.0 0.0 2.0 1.0
+#pragma parameter a_MSIZE "Mask Size" 1.0 1.0 2.0 1.0
+#pragma parameter a_MASK "Mask Strength" 0.2 0.0 0.5 0.05
+#pragma parameter bogus3 " [ GEOMETRY ] " 0.0 0.0 0.0 0.0
+#pragma parameter a_sharper "Sharp Image" 0.0 0.0 1.0 1.0
+#pragma parameter a_lanc "Lanczos Fake Artifacts" 1.0 0.0 1.0 1.0
+#pragma parameter warpx "Curvature Horizontal" 0.03 0.0 0.2 0.01
+#pragma parameter warpy "Curvature Vertical" 0.04 0.0 0.2 0.01
+#pragma parameter a_corner "Corner Roundness" 0.03 0.0 0.2 0.01
+#pragma parameter bsmooth "Border Smoothness" 600.0 100.0 1000.0 25.0
+#pragma parameter a_vignette "Vignette On/Off" 1.0 0.0 1.0 1.0
+#pragma parameter a_vigstr "Vignette Strength" 0.4 0.0 1.0 0.05
 
-#define pi 3.141592
-#define out_gamma  vec4(vec3(1.0 / monitor_gamma), 1.0)
-#define scale vec4(TextureSize/InputSize,InputSize/TextureSize)
+#define SourceSize vec4(TextureSize.xy, 1.0/TextureSize.xy)
+#define scale vec2(SourceSize.xy/InputSize.xy)
+#define pi 3.1415926
 
 #if defined(VERTEX)
 
@@ -25,8 +35,8 @@
 #define COMPAT_ATTRIBUTE in
 #define COMPAT_TEXTURE texture
 #else
-#define COMPAT_VARYING varying 
-#define COMPAT_ATTRIBUTE attribute 
+#define COMPAT_VARYING varying
+#define COMPAT_ATTRIBUTE attribute
 #define COMPAT_TEXTURE texture2D
 #endif
 
@@ -41,10 +51,10 @@ COMPAT_ATTRIBUTE vec4 COLOR;
 COMPAT_ATTRIBUTE vec4 TexCoord;
 COMPAT_VARYING vec4 COL0;
 COMPAT_VARYING vec4 TEX0;
-COMPAT_VARYING float fragpos;
-COMPAT_VARYING float aspect;
+COMPAT_VARYING vec2 ps;
+COMPAT_VARYING float maskpos;
 
-vec4 _oPosition1; 
+vec4 _oPosition1;
 uniform mat4 MVPMatrix;
 uniform COMPAT_PRECISION int FrameDirection;
 uniform COMPAT_PRECISION int FrameCount;
@@ -54,34 +64,25 @@ uniform COMPAT_PRECISION vec2 InputSize;
 
 // compatibility #defines
 #define vTexCoord TEX0.xy
-#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
-#define OutSize vec4(OutputSize, 1.0 / OutputSize)
 
 #ifdef PARAMETER_UNIFORM
-uniform COMPAT_PRECISION float WHATEVER;
+// All parameter floats need to have COMPAT_PRECISION in front of them
+uniform COMPAT_PRECISION float a_MSIZE;
+
 #else
-#define WHATEVER 0.0
+#define a_MSIZE 1.0
+
 #endif
 
 void main()
 {
     gl_Position = MVPMatrix * VertexCoord;
     TEX0.xy = TexCoord.xy*1.0001;
-    fragpos = TEX0.x*OutputSize.x*scale.x*pi;
-    aspect = InputSize.y/InputSize.x;
+    ps = 1.0/TextureSize.xy;
+    maskpos = vTexCoord.x*OutputSize.x/a_MSIZE*scale.x*pi;
 }
 
 #elif defined(FRAGMENT)
-
-#if __VERSION__ >= 130
-#define COMPAT_VARYING in
-#define COMPAT_TEXTURE texture
-out vec4 FragColor;
-#else
-#define COMPAT_VARYING varying
-#define FragColor gl_FragColor
-#define COMPAT_TEXTURE texture2D
-#endif
 
 #ifdef GL_ES
 #ifdef GL_FRAGMENT_PRECISION_HIGH
@@ -94,6 +95,16 @@ precision mediump float;
 #define COMPAT_PRECISION
 #endif
 
+#if __VERSION__ >= 130
+#define COMPAT_VARYING in
+#define COMPAT_TEXTURE texture
+out COMPAT_PRECISION vec4 FragColor;
+#else
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
+#define COMPAT_TEXTURE texture2D
+#endif
+
 uniform COMPAT_PRECISION int FrameDirection;
 uniform COMPAT_PRECISION int FrameCount;
 uniform COMPAT_PRECISION vec2 OutputSize;
@@ -101,144 +112,134 @@ uniform COMPAT_PRECISION vec2 TextureSize;
 uniform COMPAT_PRECISION vec2 InputSize;
 uniform sampler2D Texture;
 COMPAT_VARYING vec4 TEX0;
-COMPAT_VARYING float fragpos;
-COMPAT_VARYING float aspect;
+COMPAT_VARYING vec2 ps;
+COMPAT_VARYING float maskpos;
 
 // compatibility #defines
 #define Source Texture
 #define vTexCoord TEX0.xy
 
-#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
-#define OutSize vec4(OutputSize, 1.0 / OutputSize)
-#define iTimer (float(FrameCount)*60.0)
-
 #ifdef PARAMETER_UNIFORM
-uniform COMPAT_PRECISION float sharpx;
-uniform COMPAT_PRECISION float sharpy;
-uniform COMPAT_PRECISION float SCANLINE_SINE_COMP_B;
-uniform COMPAT_PRECISION float SIZE;
-uniform COMPAT_PRECISION float warpX;
-uniform COMPAT_PRECISION float warpY;
-uniform COMPAT_PRECISION float corner_round;
-uniform COMPAT_PRECISION float cgwg;
-uniform COMPAT_PRECISION float MSIZE;
-uniform COMPAT_PRECISION float monitor_gamma;
-uniform COMPAT_PRECISION float boost;
-uniform COMPAT_PRECISION float GLOW_LINE;
+uniform COMPAT_PRECISION float warpx;
+uniform COMPAT_PRECISION float warpy;
+uniform COMPAT_PRECISION float a_vignette;
+uniform COMPAT_PRECISION float a_vigstr;
+uniform COMPAT_PRECISION float a_gamma_in;
+uniform COMPAT_PRECISION float a_gamma_out;
+uniform COMPAT_PRECISION float a_col_temp;
+uniform COMPAT_PRECISION float a_sat;
+uniform COMPAT_PRECISION float a_boostd;
+uniform COMPAT_PRECISION float a_boostb;
+uniform COMPAT_PRECISION float a_interlace;
+uniform COMPAT_PRECISION float scanl;
+uniform COMPAT_PRECISION float scanh;
+uniform COMPAT_PRECISION float a_MASK;
+uniform COMPAT_PRECISION float a_MTYPE;
+uniform COMPAT_PRECISION float a_corner;
+uniform COMPAT_PRECISION float bsmooth;
+uniform COMPAT_PRECISION float a_sharper;
+uniform COMPAT_PRECISION float a_lanc;
 #else
-#define sharpx     2.0 
-#define sharpy     4.0
-#define SCANLINE_SINE_COMP_B 0.25
-#define SIZE 1.0
-#define warpX 0.031
-#define warpY 0.041
-#define corner_round 0.030
-#define cgwg 0.4
-#define MSIZE 1.0
-#define monitor_gamma 2.4
-#define boost 0.00
-#define GLOW_LINE 0.00
+
+#define warpx 0.0
+#define warpy 0.0
+#define a_vignette 0.0
+#define a_vigstr 0.0
+#define a_gamma_in 2.4
+#define a_gamma_out 2.2
+#define a_col_temp 0.0
+#define a_sat 1.0
+#define a_boostd 1.0
+#define a_boostb 1.0
+#define a_interlace 1.0
+#define scanl 0.4
+#define scanh 0.25
+#define a_MASK 0.15
+#define a_MTYPE 1.0
+#define a_corner 0.03
+#define bsmooth 600.0
+#define a_sharper 1.0
+#define a_lanc 1.0
 #endif
 
-
-
-// Distortion of scanlines, and end of screen alpha.
 vec2 Warp(vec2 pos)
 {
-    pos  = pos*2.0-1.0;    
-    pos *= vec2(1.0 + (pos.y*pos.y)*warpX, 1.0 + (pos.x*pos.x)*warpY);
-    
-    return pos*0.5 + 0.5;
+    pos = pos*2.0-1.0;
+    pos *= vec2(1.0+pos.y*pos.y*warpx, 1.0+pos.x*pos.x*warpy);
+    pos = pos*0.5+0.5;
+    return pos;
 }
 
 float corner(vec2 coord)
 {
-                coord *= scale.xy;
-                coord = (coord - vec2(0.5)) * 1.0 + vec2(0.5);
-                coord = min(coord, vec2(1.0)-coord) * vec2(1.0, aspect);
-                vec2 cdist = vec2(corner_round);
+                coord = min(coord, vec2(1.0)-coord);
+                vec2 cdist = vec2(a_corner);
                 coord = (cdist - min(coord,cdist));
                 float dist = sqrt(dot(coord,coord));
-                return clamp((cdist.x-dist)*300.0,0.0, 1.0);
+                return clamp((cdist.x-dist)*bsmooth,0.0, 1.0);
 }  
-
-
- float randomPass(vec2 coords)
- {
-    return fract(smoothstep(-120.0, 0.0, coords.y - (TextureSize.y + 120.0) * fract(iTimer * 0.00015)));
-}
-
-#define tex SourceSize.zw
-
-vec3 BilinearSharp (vec2 pos)
-{
-    vec2 uv = pos * SourceSize.x + 0.5;
-    
-    vec2 frac = fract(uv);
-    uv = (floor(uv) / SourceSize.x) - vec2(tex);
-
-    vec3 C11 = COMPAT_TEXTURE(Source, uv + vec2( 0.0   , 0.0)).rgb;
-    vec3 C21 = COMPAT_TEXTURE(Source, uv + vec2( tex.x , 0.0)).rgb;
-    vec3 C12 = COMPAT_TEXTURE(Source, uv + vec2( 0.0 , tex.y)).rgb;
-    vec3 C22 = COMPAT_TEXTURE(Source, uv + vec2( tex.x , tex.y)).rgb;
-
-    float x = frac.x;
-    float xx = 1.0-frac.x;
-    float y = frac.y;
-
-    float s = pow(x,sharpx);
-    float s2 = pow(xx,sharpx);
-
-    float t = pow(y,1.5);    
-
-    vec3 up = (C11*s2 + C21*s)/(s+s2); 
-    vec3 dw = (C12*s2 + C22*s)/(s+s2); 
-    return mix(up, dw, t);
-}
-
-float scan(float pos, vec3 color)
-    {
-    float wid = SCANLINE_SINE_COMP_B + 0.1 * dot(color, vec3(0.333))*0.8;
-    float weight = pos / wid;
-    return  boost + (0.1 + SCANLINE_SINE_COMP_B) * exp(-weight*weight ) / wid;
-    }
-
 
 void main()
 {
-    vec2 pos = Warp(TEX0.xy*(scale.xy))*scale.zw;
-    
-    vec4 res = vec4(BilinearSharp(pos),1.0);
+vec2 pos = Warp(vTexCoord*scale);
+vec2 cpos = pos;
+pos /= scale;
 
-    res *= res; 
+// filter
+vec2 ogl2pos = pos*SourceSize.xy;
+vec2 xy = floor(ogl2pos)+0.5;
+vec2 near = xy*ps;
+vec2 d = ogl2pos-xy;
+d = d*d*d*4.0*ps;
+d = near+d;
 
-    //crt-Geom scanlines
-    float f = fract(pos.y*SourceSize.y*SIZE-0.5);
-    res *= scan(f, res.rgb) +scan(1.0-f, res.rgb) ;
+// blurrier option
+if (a_sharper == 0.0) d = vec2(pos.x,d.y);
 
-    // apply the mask
-    float dotMaskWeights = mix(cgwg, 1.0, 0.5*sin(fragpos*MSIZE)+0.5);
-    res *= dotMaskWeights;
+vec3 res = COMPAT_TEXTURE(Source,d).rgb;
 
-    vec4 color = res;
+// fake Lanczos artifacts
+vec3 resl = COMPAT_TEXTURE(Source,d + vec2(ps.x,0.0)).rgb;
+vec3 resr = COMPAT_TEXTURE(Source,d - vec2(ps.x,0.0)).rgb;
+vec3 lanc = resl*0.5+resr*0.5;
+lanc *= lanc;
 
-    color.rgb += boost*color.rgb;
+float w = dot(vec3(0.33),res);
 
-    // re-apply the gamma curve for the mask path
-    color = pow(color, out_gamma);
+if (a_sharper == 0.0 && a_lanc == 1.0) {res -= 0.2*lanc; res *= 1.15; res = clamp(res,0.0,1.0); } 
+// color temp approximate
+res *= vec3(1.0+a_col_temp,1.0-a_col_temp*0.2,1.0-a_col_temp);
 
-    color += randomPass(pos * TextureSize) * GLOW_LINE;
+float scan = mix(scanl,scanh,w);
 
-#if defined GL_ES
-    // hacky clamp fix for GLES
-    vec2 bordertest = (pos);
-    if ( bordertest.x > 0.0001 && bordertest.x < 0.9999 && bordertest.y > 0.0001 && bordertest.y < 0.9999)
-        res = res;
-    else
-        res = vec4(0.,0.,0.,0.);
-#endif
+res = pow(res, vec3(a_gamma_in));
 
-    FragColor = color*corner(pos);
+float vig = 0.0;
+if (a_vignette == 1.0){
+vig = cpos.x-0.5;
+vig = vig*vig*a_vigstr;
+}
+// Interlace handling
+if (InputSize.y>400.0) {ogl2pos /= 2.0;
+if (mod(float(FrameCount),2.0) > 0.0 && a_interlace == 1.0) ogl2pos += 0.5;
+}
 
-} 
+res *= (scan+vig)*sin((ogl2pos.y-0.25)*2.0*pi)+(1.0-scan-vig);
+// masks
+float sz = 1.0;
+float m_m = maskpos;
+if (a_MTYPE == 1.0) sz = 0.6666;
+if (a_MTYPE == 2.0) m_m = ogl2pos.x*2.0*pi;
+res *= a_MASK*sin(m_m*sz)+1.0-a_MASK;
+
+res = pow(res,vec3(1.0/a_gamma_out));
+
+float l = dot(res,vec3(0.3,0.6,0.1));
+res = mix(vec3(l),res,a_sat);
+
+res *= mix(a_boostd,a_boostb,l);
+
+if (a_corner >0.0) res *= corner(cpos);
+FragColor.rgb = res;
+}
 #endif
