0275df018ca0864e9325a8fd8961a7c65b2486e1
metallic77 <43163462+metallic77@users.noreply.github.com>
2023-12-20 12:14:52 +0200

    Add files via upload

---
+++ ntsc/shaders/ntsc-simple/gauss_lp.glsl
@@ -0,0 +1,131 @@
+#version 110
+
+/*
+NTSC-mini DariusG 2023
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 2 of the License, or (at your option)
+any later version.
+*/
+
+#if defined(VERTEX)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING out
+#define COMPAT_ATTRIBUTE in
+#define COMPAT_TEXTURE texture
+#else
+#define COMPAT_VARYING varying 
+#define COMPAT_ATTRIBUTE attribute 
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+COMPAT_ATTRIBUTE vec4 VertexCoord;
+COMPAT_ATTRIBUTE vec4 COLOR;
+COMPAT_ATTRIBUTE vec4 TexCoord;
+COMPAT_VARYING vec4 COL0;
+COMPAT_VARYING vec4 TEX0;
+
+vec4 _oPosition1; 
+uniform mat4 MVPMatrix;
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+
+// compatibility #defines
+#define vTexCoord TEX0.xy
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
+#define OutSize vec4(OutputSize, 1.0 / OutputSize)
+
+#ifdef PARAMETER_UNIFORM
+uniform COMPAT_PRECISION float WHATEVER;
+#else
+#define WHATEVER 0.0
+#endif
+
+void main()
+{
+    gl_Position = MVPMatrix * VertexCoord;
+    TEX0.xy = TexCoord.xy*1.0001;
+}
+
+#elif defined(FRAGMENT)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING in
+#define COMPAT_TEXTURE texture
+out vec4 FragColor;
+#else
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+precision mediump float;
+#endif
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+uniform sampler2D Texture;
+COMPAT_VARYING vec4 TEX0;
+
+// compatibility #defines
+#define vTexCoord TEX0.xy
+#define Source Texture
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
+#define OutSize vec4(OutputSize, 1.0 / OutputSize)
+
+#ifdef PARAMETER_UNIFORM
+uniform COMPAT_PRECISION float compo;
+
+#else
+#define compo 1.0
+#endif
+
+void main()
+{
+    vec3 res = vec3(0.0);
+    
+    vec2 ps = vec2(SourceSize.z,0.0);
+
+    res += COMPAT_TEXTURE(Source,vTexCoord).rgb*0.2504;
+
+    res += COMPAT_TEXTURE(Source,vTexCoord-ps).rgb*0.20597;
+    
+    res += COMPAT_TEXTURE(Source,vTexCoord+ps).rgb*0.20597;
+
+    res += COMPAT_TEXTURE(Source,vTexCoord+2.0*ps).rgb*0.11464;
+
+    res += COMPAT_TEXTURE(Source,vTexCoord-2.0*ps).rgb*0.11464;
+
+    res += COMPAT_TEXTURE(Source,vTexCoord+3.0*ps).rgb*0.04317;
+
+    res += COMPAT_TEXTURE(Source,vTexCoord-3.0*ps).rgb*0.04317;
+    
+    res += COMPAT_TEXTURE(Source,vTexCoord+4.0*ps).rgb*0.011;
+
+    res += COMPAT_TEXTURE(Source,vTexCoord-4.0*ps).rgb*0.011;
+
+    FragColor = vec4(res,0);
+}
+#endif
