50919660c13b8e43542c195d5bab325243e37150
metallic77 <43163462+metallic77@users.noreply.github.com>
2024-02-25 09:01:09 +0200

    Update ntsc-mini-1.glsl

--- ntsc/shaders/ntsc-simple/ntsc-mini-1.glsl
+++ ntsc/shaders/ntsc-simple/ntsc-mini-1.glsl
@@ -24,6 +24,7 @@ COMPAT_ATTRIBUTE vec4 COLOR;
 COMPAT_ATTRIBUTE vec4 TexCoord;
 COMPAT_VARYING vec4 COL0;
 COMPAT_VARYING vec4 TEX0;
+COMPAT_VARYING vec2 scale;
 
 vec4 _oPosition1; 
 uniform mat4 MVPMatrix;
@@ -48,6 +49,7 @@ void main()
 {
     gl_Position = MVPMatrix * VertexCoord;
     TEX0.xy = TexCoord.xy*1.0001;
+    scale = SourceSize.xy/InputSize.xy;
 }
 
 #elif defined(FRAGMENT)
@@ -80,6 +82,7 @@ uniform COMPAT_PRECISION vec2 TextureSize;
 uniform COMPAT_PRECISION vec2 InputSize;
 uniform sampler2D Texture;
 COMPAT_VARYING vec4 TEX0;
+COMPAT_VARYING vec2 scale;
 
 // compatibility #defines
 #define vTexCoord TEX0.xy
@@ -99,7 +102,7 @@ uniform COMPAT_PRECISION float animate_ph;
 // This pass converts RGB colors  to
 // a YIQ (NTSC) Composite signal.
 
-#define PI   3.14159265358979323846*2.0/3.0
+#define PI   3.14159265358979323846
 #define TAU  6.28318530717958647693
 
 const mat3 yiq_to_rgb = mat3(1.000, 1.000, 1.000,
@@ -110,26 +113,31 @@ void main() {
     vec3 rgb = vec3(0.0);
     float sum = 0.0;
     vec2 ps = vec2(SourceSize.z,0.0);
-    
-    for (int i=-4; i<4; i++)
+    for (int i=-4; i<=4; i++)
     {    
     float offset = float(i);
-    float w= exp(-0.1*offset*offset);
-    float phase = vTexCoord.x*SourceSize.x + vTexCoord.y*SourceSize.y*2.0 + offset;
-    
-    if (animate_ph == 1.0) phase += mod(float(FrameCount),3.0)*PI;
-    float cs = cos(phase*PI);
-    float sn = sin(phase*PI);
-    
-    if ( offset<1.0 && offset>-1.0) rgb.r += COMPAT_TEXTURE(Source,vTexCoord+ps*offset).r;
-    rgb.yz += COMPAT_TEXTURE(Source,vTexCoord + ps*offset).gb*2.0*vec2(cs,sn)*w;
-    sum += w;
+    if ( offset>-3.0  && offset<=2.0 ) {
+    // Low Pass Y    
+        float w= exp(-0.2*offset*offset);
+        rgb.r += COMPAT_TEXTURE(Source,vTexCoord + ps*offset).r*w;
+        sum += w;
     }
-    
-    rgb.yz /= sum;
-    rgb.x /= 1.0;
-    rgb *= yiq_to_rgb;
+    float phase = (vTexCoord.x*SourceSize.x + offset)*PI*0.666 + mod(vTexCoord.y*SourceSize.y*0.666,2.0)*PI;
+    float time = animate_ph > 0.0? (float(FrameCount))*PI:0.0;
 
-    FragColor = vec4(vec3(rgb), 1.0);
+    float cs = cos(phase+time);
+    float sn = sin(phase+time);
+   
+    // High Pass Chroma (1.0 - lowpass), crude separation like a very early TV
+    float w = exp2(-0.3*offset*offset);
+    float r = 1.0-w;
+    rgb.yz += r*COMPAT_TEXTURE(Source,vTexCoord  + ps*offset).gb*3.0*vec2(cs,sn);
+    }
+    rgb.x /= sum;
+    rgb.yz /= 9.0;
+
+    rgb *= yiq_to_rgb;
+ 
+    FragColor = vec4(rgb, 1.0);
 }
 #endif 
