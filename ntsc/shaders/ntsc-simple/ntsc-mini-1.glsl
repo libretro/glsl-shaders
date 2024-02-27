4137048d1c6bdba19a52aa609b73291b22e65f5c
metallic77 <43163462+metallic77@users.noreply.github.com>
2024-02-27 21:37:38 +0200

    update ntsc-mini with comb filter (#404)
    * Update ntsc-mini-0.glsl
    
    * Update ntsc-mini-1.glsl

--- ntsc/shaders/ntsc-simple/ntsc-mini-1.glsl
+++ ntsc/shaders/ntsc-simple/ntsc-mini-1.glsl
@@ -1,6 +1,9 @@
 #version 110
 
-#pragma parameter animate_ph "Animate Phase" 1.0 0.0 1.0 1.0
+#pragma parameter crawl "Dot Crawl (Genesis off)" 0.0 0.0 1.0 1.0
+#pragma parameter Y_lp "Luma Low Pass (sharper)" 0.08 0.0 1.0 0.01
+#pragma parameter pi_mod "Pi modifier 1' degree-step" 0.633319 0.5 1.0 0.005555
+#pragma parameter dummy "Genesis 0.53" 0.0 0.0 0.0 0.0
 #if defined(VERTEX)
 
 #if __VERSION__ >= 130
@@ -24,7 +27,6 @@ COMPAT_ATTRIBUTE vec4 COLOR;
 COMPAT_ATTRIBUTE vec4 TexCoord;
 COMPAT_VARYING vec4 COL0;
 COMPAT_VARYING vec4 TEX0;
-COMPAT_VARYING vec2 scale;
 
 vec4 _oPosition1; 
 uniform mat4 MVPMatrix;
@@ -49,7 +51,6 @@ void main()
 {
     gl_Position = MVPMatrix * VertexCoord;
     TEX0.xy = TexCoord.xy*1.0001;
-    scale = SourceSize.xy/InputSize.xy;
 }
 
 #elif defined(FRAGMENT)
@@ -82,7 +83,6 @@ uniform COMPAT_PRECISION vec2 TextureSize;
 uniform COMPAT_PRECISION vec2 InputSize;
 uniform sampler2D Texture;
 COMPAT_VARYING vec4 TEX0;
-COMPAT_VARYING vec2 scale;
 
 // compatibility #defines
 #define vTexCoord TEX0.xy
@@ -91,53 +91,60 @@ COMPAT_VARYING vec2 scale;
 #define OutSize vec4(OutputSize, 1.0 / OutputSize)
 
 #ifdef PARAMETER_UNIFORM
-uniform COMPAT_PRECISION float animate_ph;
+uniform COMPAT_PRECISION float crawl;
+uniform COMPAT_PRECISION float Y_lp;
+uniform COMPAT_PRECISION float pi_mod;
 
 #else
-#define animate_ph 1.0
+#define crawl 1.0
+#define Y_lp 0.2
+#define pi_mod 0.5
 #endif
-
-
-// Encoder or Modulator
-// This pass converts RGB colors  to
-// a YIQ (NTSC) Composite signal.
 
 #define PI   3.14159265358979323846
 #define TAU  6.28318530717958647693
 
-const mat3 yiq_to_rgb = mat3(1.000, 1.000, 1.000,
-                             0.956,-0.272,-1.106,
-                             0.621,-0.647, 1.703);
+const mat3 YUV2RGB = mat3(1.0, 0.0, 1.13983,
+                          1.0, -0.39465, -0.58060,
+                          1.0, 2.03211, 0.0);
 
-void main() {
-    vec3 rgb = vec3(0.0);
-    float sum = 0.0;
-    vec2 ps = vec2(SourceSize.z,0.0);
-    for (int i=-4; i<=4; i++)
-    {    
-    float offset = float(i);
-    if ( offset>-3.0  && offset<=2.0 ) {
-    // Low Pass Y    
-        float w= exp(-0.2*offset*offset);
-        rgb.r += COMPAT_TEXTURE(Source,vTexCoord + ps*offset).r*w;
+
+void main() 
+{
+vec2 dx = vec2(SourceSize.z,0.0);
+vec2 dy = vec2(0.0,SourceSize.w*0.5);
+vec3 res = vec3(0.0);
+float sum = 0.0;
+float sumc = 0.0;
+
+// Comb-filter YC separation idea borrowed from ntsc-blastem
+for (int x=0; x<4; x++)
+    {
+        float n = float(x);
+        float w = exp(-Y_lp*n*n);
+        // add a lowpass to Luma too  
+        vec3 line = w*COMPAT_TEXTURE(Source,vTexCoord + n*dx).rgb;        
+        vec3 lineup = w*COMPAT_TEXTURE(Source,vTexCoord -dy + n*dx).rgb;        
         sum += w;
+        // Comb-filter separate, idea borrowed from blastem and tweaked to ntsc-mini 
+        vec3 ymix = (line+lineup)*0.5;
+        res.r += ymix.r;
     }
-    float phase = (vTexCoord.x*SourceSize.x + offset)*PI*0.666 + mod(vTexCoord.y*SourceSize.y*0.666,2.0)*PI;
-    float time = animate_ph > 0.0? (float(FrameCount))*PI:0.0;
+    res.r /= sum;
+for (int a=0; a<7; a++)
+    {
+        float b = float(a);  
+        float phase = (vTexCoord.x*SourceSize.x + b)*PI*pi_mod + mod(vTexCoord.y*SourceSize.y,2.0)*PI*0.5 ;
+        if (crawl == 1.0) phase += sin(mod(float(FrameCount),2.0))*PI;
+        vec3 carr = vec3(1.0,2.0*cos(phase),2.0*sin(phase));
+        float w = 1.0-exp(-0.4*b*b);  
 
-    float cs = cos(phase+time);
-    float sn = sin(phase+time);
-   
-    // High Pass Chroma (1.0 - lowpass), crude separation like a very early TV
-    float w = exp2(-0.3*offset*offset);
-    float r = 1.0-w;
-    rgb.yz += r*COMPAT_TEXTURE(Source,vTexCoord  + ps*offset).gb*3.0*vec2(cs,sn);
+        vec3 cline   = COMPAT_TEXTURE(Source,vTexCoord + b*dx).rgb*carr;        
+        vec3 clineup = COMPAT_TEXTURE(Source,vTexCoord -dy + b*dx).rgb*carr;  
+        // Comb-filter separate, idea borrowed from blastem and tweaked to ntsc-mini 
+        vec3 iqmix = (cline - (cline+clineup));
+        res.gb += iqmix.gb/7.0;
     }
-    rgb.x /= sum;
-    rgb.yz /= 9.0;
-
-    rgb *= yiq_to_rgb;
- 
-    FragColor = vec4(rgb, 1.0);
+    FragColor.rgb = res*YUV2RGB;
 }
 #endif 
