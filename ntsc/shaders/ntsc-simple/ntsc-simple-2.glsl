00ccbf4f5b2b43e4191ce0e31331d5af23d8dc3e
metallic77 <43163462+metallic77@users.noreply.github.com>
2024-07-10 22:29:12 +0300

    ntsc-mini small improvements (#465)
    * Update ntsc-mini.glslp
    
    * Update ntsc-mini-0.glsl
    
    * Update ntsc-simple-2.glsl

--- ntsc/shaders/ntsc-simple/ntsc-simple-2.glsl
+++ ntsc/shaders/ntsc-simple/ntsc-simple-2.glsl
@@ -1,26 +1,13 @@
 #version 110
 
-/*
-   Simple composite-video (more like RF) like shader by DariusG 2023
-   
-   This program is free software; you can redistribute it and/or modify it
-   under the terms of the GNU General Public License as published by the Free
-   Software Foundation; either version 2 of the License, or (at your option)
-   any later version.
-*/
-
-#pragma parameter ntsc_sat "NTSC Saturation" 3.0 0.0 6.0 0.05
-#pragma parameter y_width "Luma Width (Blurry)" 4.0 1.0 8.0 1.0
-#pragma parameter iq_width "Chroma Width (Bleed)" 9.0 4.0 32.0 1.0
-#pragma parameter afacts "Low Pass Luma" 0.05 0.0 1.0 0.01
-#pragma parameter h_pass_c "High Pass Chroma" 0.05 0.01 1.0 0.01
-#pragma parameter animate_afacts "NTSC Artifacts Animate" 0.0 0.0 1.0 1.0
-#pragma parameter phase_shifti "Phase Shift I" 0.0 -5.0 5.0 0.05
-#pragma parameter phase_shiftq "Phase Shift Q" 0.0 -5.0 5.0 0.05
-#pragma parameter comp_rf "Composite/RF" 0.0 0.0 1.0 1.0
-#pragma parameter rf_noise "RF noise" 0.05 0.0 1.0 0.01
-#pragma parameter x_mod "PI x mod" 0.59 0.0 2.0 0.01
-#pragma parameter dummy "ZX Spectrum:0.59, Gen:0.76" 0.0 0.0 0.0 0.0
+#pragma parameter ph_mode "Phase: 1:ZX,2:MD,3:NES/SNES,4:Artifacts" 2.0 0.0 4.0 1.0
+#pragma parameter mini_sharp "Resolution" 1.0 0.1 4.0 0.1
+#pragma parameter Fl "Freq. Cutoff" 0.2 0.01 1.0 0.01
+#pragma parameter lpass "Chroma Low Pass" 0.05 0.0 1.0 0.01
+#pragma parameter d_crawl "Dot Crawl" 0.0 0.0 1.0 1.0
+#pragma parameter mini_hue1 "Hue Shift I" 0.1 -6.0 6.0 0.05
+#pragma parameter mini_hue2 "Hue Shift Q" -0.1 -6.0 6.0 0.05
+#pragma parameter mini_sat "Saturation" 2.0 0.0 4.0 0.05
 
 #if defined(VERTEX)
 
@@ -103,100 +90,93 @@ uniform sampler2D Texture;
 COMPAT_VARYING vec4 TEX0;
 
 // compatibility #defines
-#define Source Texture
 #define vTexCoord TEX0.xy
-
+#define Source Texture
 #define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
 #define OutSize vec4(OutputSize, 1.0 / OutputSize)
 
 #ifdef PARAMETER_UNIFORM
-uniform COMPAT_PRECISION float ntsc_sat;
-uniform COMPAT_PRECISION float afacts;
-uniform COMPAT_PRECISION float animate_afacts;
-uniform COMPAT_PRECISION float phase_shifti;
-uniform COMPAT_PRECISION float phase_shiftq;
-uniform COMPAT_PRECISION float iq_width;
-uniform COMPAT_PRECISION float y_width;
-uniform COMPAT_PRECISION float comp_rf;
-uniform COMPAT_PRECISION float h_pass_c;
-uniform COMPAT_PRECISION float x_mod;
-uniform COMPAT_PRECISION float rf_noise;
+uniform COMPAT_PRECISION float ph_mode;
+uniform COMPAT_PRECISION float Fl;
+uniform COMPAT_PRECISION float lpass;
+uniform COMPAT_PRECISION float d_crawl;
+uniform COMPAT_PRECISION float mini_hue;
+uniform COMPAT_PRECISION float mini_sat;
+uniform COMPAT_PRECISION float mini_sharp;
 
 #else
-#define ntsc_sat 1.0
-#define afacts 0.0
-#define animate_afacts 0.0
-#define phase_shifti 0.0
-#define phase_shiftq 0.0
-#define iq_width 8.0
-#define y_width 2.0
-#define comp_rf 0.0
-#define h_pass_c 0.05
-#define x_mod 0.05
-#define rf_noise 0.05
+#define ph_mode 90.0
+#define Fl 90.0
+#define lpass 0.2
+#define d_crawl 0.0
+#define mini_hue 0.0
+#define mini_sat 0.0
+#define mini_sharp 1.0
 #endif
 
-// this pass is a modification of https://www.shadertoy.com/view/3t2XRV
-
+#define PI   3.14159265358979323846
 #define TAU  6.28318530717958647693
-#define PI 3.1415926
+#define s 1.0
+#define onedeg 0.017453
 
-// Colorspace conversion matrix for YUV-to-RGB
-// All modern CRTs use YUV instead of YIQ
 const mat3 YUV2RGB = mat3(1.0, 0.0, 1.13983,
                           1.0, -0.39465, -0.58060,
                           1.0, 2.03211, 0.0);
 
-float hann(float i, int size, float phase) {
-    return pow(sin((PI * (i + phase)) / float(size)), 2.0);
-}
-
-#define iTimer float(FrameCount)
-
-float noise(vec2 co)
+float kaizer (float N, float p)
 {
-return fract(sin(iTimer * dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+    // Compute sinc filter.
+    float k = sin(2.0 * Fl / s * (N - (p - 1.0) / 2.0));
+    return k;
 }
 
-void main()
+void main() {
+
+vec3 yuv = vec3(0.0);
+vec2 ps = vec2(SourceSize.z,0.0);
+float sum = 0.0; float sumc = 0.0;
+
+
+// luma
+for (int i=0; i<4; i++)
 {
-vec2 size = SourceSize.xy;
-vec2 uv = vTexCoord;
-int a = int(iq_width);
-int b = int(y_width);
-
-    //Sample composite signal and decode to YUV
-    vec3 YUV = vec3(0);
-    float sum = 0.0;
-    for (int n=-b; n<b; n++) {
-        // lowpass
-        float w = exp(-afacts*float(n)*float(n));
-        vec2 pos = uv + vec2(float(n) / size.x/2.0, 0.0);
-        // low pass Y signal, high frequency chroma pattern is cut-off
-        YUV.x += COMPAT_TEXTURE(Source, pos).r*w ;
-        sum += w;
-        }
-        YUV.x /= sum;
-    float sumc = 0.0;
-    for (int n=-a; n<a; n++) {
-        vec2 pos = uv + vec2(float(n) / size.x, 0.0);
-        float phase = (floor(vTexCoord.x*SourceSize.x + float(n)))*PI*x_mod - mod(floor(vTexCoord.y*SourceSize.y),2.0)*PI ;
-    // High Pass Chroma
-    float r = exp(-h_pass_c*float(n)*float(n));
-    //animate to further hide artifacts
-    if (animate_afacts == 1.0) phase += PI*sin(mod(float(FrameCount+1),2.0));
-    // add optional hann window function
-    vec2 carrier;
-    if (comp_rf == 0.0) carrier = ntsc_sat*vec2(sin(phase+phase_shifti), cos(phase+phase_shiftq));
-    else carrier = ntsc_sat*4.0*vec2(sin(phase+phase_shifti), cos(phase+phase_shiftq))*hann(float(n),a*4,0.0);
-        YUV.yz += r*COMPAT_TEXTURE(Source, pos).gb * carrier;
-        sumc += r;
-        }
-    YUV.yz /= sumc;
-
-    //  Convert signal to RGB
-    YUV = YUV*YUV2RGB;
-    FragColor = vec4(YUV*(1.0+noise(vTexCoord)*rf_noise), 1.0);
-    
+float p = float (i);
+vec2 pos = vTexCoord + ps*p/mini_sharp -ps;
+// Window
+float w = kaizer(4.0,p);
+yuv.r += COMPAT_TEXTURE(Source,pos).r*w;
+sum += w;
 }
-#endif
+yuv.r /= sum;
+
+vec3 line = vec3(0.0);
+
+//chroma
+for (int i=-4; i<4; i++)
+{
+float p = float (i);
+// Low-pass 
+float w = exp(-lpass*p*p);
+
+float h_ph, v_ph, mod0 = 0.0;
+if      (ph_mode == 0.0) {h_ph =  90.0*onedeg; v_ph = PI*0.6667; mod0 = 2.0;}
+else if (ph_mode == 1.0) {h_ph = 110.0*onedeg; v_ph = PI;        mod0 = 2.0;}
+else if (ph_mode == 2.0) {h_ph = 132.0*onedeg; v_ph = PI;        mod0 = 2.0;}
+else if (ph_mode == 3.0) {h_ph =  90.0*onedeg; v_ph = PI*0.6667;    mod0 = 4.0;}
+else                     {h_ph =  90.0*onedeg; v_ph = PI;        mod0 = 1.0;}
+
+float phase = floor(vTexCoord.x*SourceSize.x + p)*h_ph + floor(vTexCoord.y*SourceSize.y)*v_ph;
+phase += mini_hue;
+phase += d_crawl *sin(mod(float(FrameCount/2),2.0))*PI;
+
+vec2 qam = mini_sat*vec2(cos(phase),sin(phase));
+line.gb = COMPAT_TEXTURE(Source,vTexCoord + ps*p).gb*qam*w;
+
+yuv.gb += line.gb;
+sumc += w;
+}
+yuv.gb /= sumc;
+
+FragColor.rgb = yuv*YUV2RGB;
+}
+#endif 
