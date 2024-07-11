c198861334ab517af67081b091afa47fa9a7262d
metallic77 <43163462+metallic77@users.noreply.github.com>
2024-07-11 11:56:44 +0300

    Update ntsc-mini-0.glsl

--- ntsc/shaders/ntsc-simple/ntsc-mini-0.glsl
+++ ntsc/shaders/ntsc-simple/ntsc-mini-0.glsl
@@ -115,7 +115,7 @@ float h_ph, v_ph, mod0 = 0.0;
 if      (ph_mode == 0.0) {h_ph =  90.0*onedeg; v_ph = PI*0.6667; mod0 = 2.0;}
 else if (ph_mode == 1.0) {h_ph = 110.0*onedeg; v_ph = PI;        mod0 = 2.0;}
 else if (ph_mode == 2.0) {h_ph = 132.0*onedeg; v_ph = PI;        mod0 = 2.0;}
-else if (ph_mode == 3.0) {h_ph =  90.0*onedeg; v_ph = PI*0.6667;    mod0 = 4.0;}
+else if (ph_mode == 3.0) {h_ph =  90.0*onedeg; v_ph = PI*0.6667;    mod0 = 3.0;}
 else                     {h_ph =  90.0*onedeg; v_ph = PI;        mod0 = 1.0;}
 
 float phase = floor(vTexCoord.x*SourceSize.x)*h_ph + floor(vTexCoord.y*SourceSize.y)*v_ph;
