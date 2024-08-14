6e95343f27a65226e6ceaa8dd169a0d4943bb71a
metallic77 <43163462+metallic77@users.noreply.github.com>
2024-08-14 10:36:14 +0300

    Update ntsc-simple-2.glsl

--- ntsc/shaders/ntsc-simple/ntsc-simple-2.glsl
+++ ntsc/shaders/ntsc-simple/ntsc-simple-2.glsl
@@ -1,13 +1,9 @@
 #version 110
 
-#pragma parameter ph_mode "Phase: 1:ZX,2:MD,3:NES/SNES,4:Artifacts" 2.0 0.0 4.0 1.0
-#pragma parameter mini_sharp "Resolution" 1.0 0.1 4.0 0.1
-#pragma parameter Fl "Freq. Cutoff" 0.2 0.01 1.0 0.01
-#pragma parameter lpass "Chroma Low Pass" 0.05 0.0 1.0 0.01
-#pragma parameter d_crawl "Dot Crawl" 0.0 0.0 1.0 1.0
-#pragma parameter mini_hue1 "Hue Shift I" 0.1 -6.0 6.0 0.05
-#pragma parameter mini_hue2 "Hue Shift Q" -0.1 -6.0 6.0 0.05
-#pragma parameter mini_sat "Saturation" 2.0 0.0 4.0 0.05
+#pragma parameter luma_cut "Luma Frequency Cut-Off" 0.15 0.01 1.0 0.01
+#pragma parameter chroma_cut "Chroma Frequency Cut-Off" 0.1 0.0 1.0 0.01
+#pragma parameter d_cr "Dot Crawl (snes,nes,pce)" 0.0 0.0 1.0 1.0
+#pragma parameter sim_ntsc "Saturation" 1.5 0.0 2.0 0.05
 
 #if defined(VERTEX)
 
@@ -96,87 +92,61 @@ COMPAT_VARYING vec4 TEX0;
 #define OutSize vec4(OutputSize, 1.0 / OutputSize)
 
 #ifdef PARAMETER_UNIFORM
-uniform COMPAT_PRECISION float ph_mode;
-uniform COMPAT_PRECISION float Fl;
-uniform COMPAT_PRECISION float lpass;
-uniform COMPAT_PRECISION float d_crawl;
-uniform COMPAT_PRECISION float mini_hue;
-uniform COMPAT_PRECISION float mini_sat;
-uniform COMPAT_PRECISION float mini_sharp;
-
+uniform COMPAT_PRECISION float luma_cut;
+uniform COMPAT_PRECISION float chroma_cut;
+uniform COMPAT_PRECISION float d_cr;
+uniform COMPAT_PRECISION float sim_ntsc;
 #else
-#define ph_mode 90.0
-#define Fl 90.0
-#define lpass 0.2
-#define d_crawl 0.0
-#define mini_hue 0.0
-#define mini_sat 0.0
-#define mini_sharp 1.0
+#define luma_cut 0.0
+#define chroma_cut 0.0
+#define d_cr 0.0
+#define sim_ntsc 0.0
 #endif
+
+
 
 #define PI   3.14159265358979323846
 #define TAU  6.28318530717958647693
-#define s 1.0
-#define onedeg 0.017453
 
 const mat3 YUV2RGB = mat3(1.0, 0.0, 1.13983,
                           1.0, -0.39465, -0.58060,
                           1.0, 2.03211, 0.0);
 
+
 float kaizer (float N, float p)
 {
     // Compute sinc filter.
-    float k = sin(2.0 * Fl / s * (N - (p - 1.0) / 2.0));
+    float k = sin(2.0 * luma_cut  * (N - (p - 1.0) / 2.0));
     return k;
 }
 
 void main() {
-
-vec3 yuv = vec3(0.0);
 vec2 ps = vec2(SourceSize.z,0.0);
-float sum = 0.0; float sumc = 0.0;
+vec3 res = vec3(0.0);
+float sum = 0.0;
+float sumc = 0.0;
+float phase = 0.0;
 
-
-// luma
-for (int i=0; i<4; i++)
+vec2 carrier;
+for (int i=-8; i<8; i++)
 {
-float p = float (i);
-vec2 pos = vTexCoord + ps*p/mini_sharp -ps;
-// Window
-float w = kaizer(4.0,p);
-yuv.r += COMPAT_TEXTURE(Source,pos).r*w;
+float n = float(i);
+float w = kaizer(4.0,n);
+float cw = exp(-chroma_cut*n*n);
+
+float iTimer = 0.0 ;
+if (d_cr == 1.0) iTimer = mod(float(FrameCount/2),2.0);
+
+phase = ((vTexCoord.x*SourceSize.x + n))*PI*0.5 +  mod(vTexCoord.y*InputSize.y*4.0+iTimer,2.0)*PI;
+carrier = vec2(sim_ntsc*cos(phase),sim_ntsc*sin(phase));
+
+res.r += COMPAT_TEXTURE(Source,vTexCoord + n*ps*0.5).r*w;
+res.gb += COMPAT_TEXTURE(Source,vTexCoord + n*ps).gb*carrier*cw;
 sum += w;
+sumc += cw;
 }
-yuv.r /= sum;
-
-vec3 line = vec3(0.0);
-
-//chroma
-for (int i=-4; i<4; i++)
-{
-float p = float (i);
-// Low-pass 
-float w = exp(-lpass*p*p);
-
-float h_ph, v_ph, mod0 = 0.0;
-if      (ph_mode == 0.0) {h_ph =  90.0*onedeg; v_ph = PI*0.6667; mod0 = 2.0;}
-else if (ph_mode == 1.0) {h_ph = 110.0*onedeg; v_ph = PI;        mod0 = 2.0;}
-else if (ph_mode == 2.0) {h_ph = 132.0*onedeg; v_ph = PI;        mod0 = 2.0;}
-else if (ph_mode == 3.0) {h_ph =  90.0*onedeg; v_ph = PI*0.6667;    mod0 = 4.0;}
-else                     {h_ph =  90.0*onedeg; v_ph = PI;        mod0 = 1.0;}
-
-float phase = floor(vTexCoord.x*SourceSize.x + p)*h_ph + floor(vTexCoord.y*SourceSize.y)*v_ph;
-phase += mini_hue;
-phase += d_crawl *sin(mod(float(FrameCount/2),2.0))*PI;
-
-vec2 qam = mini_sat*vec2(cos(phase),sin(phase));
-line.gb = COMPAT_TEXTURE(Source,vTexCoord + ps*p).gb*qam*w;
-
-yuv.gb += line.gb;
-sumc += w;
+res.r /= sum;
+res.gb /= sumc;
+FragColor.rgb = res*YUV2RGB;
 }
-yuv.gb /= sumc;
-
-FragColor.rgb = yuv*YUV2RGB;
-}
-#endif 
+#endif
