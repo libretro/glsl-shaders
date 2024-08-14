b7d047a9c58e74cf3844dea8191e823ddf0c4f0b
metallic77 <43163462+metallic77@users.noreply.github.com>
2024-08-14 21:29:11 +0300

    update ntsc-simple (#478)
    * Update ntsc-simple.glslp
    
    * Update ntsc-simple-1.glsl
    
    * Update ntsc-simple-2.glsl

--- ntsc/shaders/ntsc-simple/ntsc-simple-1.glsl
+++ ntsc/shaders/ntsc-simple/ntsc-simple-1.glsl
@@ -1,17 +1,5 @@
 #version 110
 
-/*
-   Simple composite-video (more like RF) like shader by DariusG 2023
-   
-   This program is free software; you can redistribute it and/or modify it
-   under the terms of the GNU General Public License as published by the Free
-   Software Foundation; either version 2 of the License, or (at your option)
-   any later version.
-*/
-
-#pragma parameter ntsc_bri "NTSC Brightness" 1.0 0.0 2.0 0.01
-#pragma parameter ntsc_hue "NTSC Hue" -0.2 -2.0 2.0 0.05
-
 #if defined(VERTEX)
 
 #if __VERSION__ >= 130
@@ -93,47 +81,38 @@ uniform sampler2D Texture;
 COMPAT_VARYING vec4 TEX0;
 
 // compatibility #defines
-#define Source Texture
 #define vTexCoord TEX0.xy
-
+#define Source Texture
 #define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
 #define OutSize vec4(OutputSize, 1.0 / OutputSize)
 
 #ifdef PARAMETER_UNIFORM
-uniform COMPAT_PRECISION float ntsc_bri;
-uniform COMPAT_PRECISION float ntsc_hue;
-uniform COMPAT_PRECISION float animate_afacts;
-uniform COMPAT_PRECISION float x_mod;
-
+uniform COMPAT_PRECISION float d_cr;
 #else
-#define ntsc_bri 1.0
-#define ntsc_hue 0.0
-#define animate_afacts 1.0
-#define x_mod 0.0
+#define d_cr 0.0
 #endif
 
 
+#define PI   3.14159265358979323846
 #define TAU  6.28318530717958647693
-#define PI 3.1415926
 
-
-// Colorspace conversion matrix for RGB-to-YUV
-// All modern CRTs use YUV instead of YIQ
 const mat3 RGBYUV = mat3(0.299, 0.587, 0.114,
                         -0.299, -0.587, 0.886, 
                          0.701, -0.587, -0.114);
 
-void main()
-{
-    float phase = floor(vTexCoord.x*SourceSize.x)*PI*x_mod - mod(floor(vTexCoord.y*SourceSize.y),2.0)*PI; 
-    phase += ntsc_hue;
-    vec3 YUV = COMPAT_TEXTURE(Source,vTexCoord).rgb; 
-    
-     YUV = YUV*RGBYUV;
-    
-    if (animate_afacts == 1.0) phase += PI*sin(mod(float(FrameCount+1),2.0));
-    float signal = ntsc_bri*YUV.x + 0.5*(YUV.y*sin(phase) + YUV.z*cos(phase)) ;   
-    FragColor = vec4(vec3(signal), 1.0);
-    
+
+void main() {
+
+vec3 res = vec3(0.0);
+float iTimer = 0.0 ;
+if (d_cr == 1.0) iTimer = mod(float(FrameCount/2),2.0);
+float phase = (vTexCoord.x*SourceSize.x)*PI*0.5+ mod(vTexCoord.y*InputSize.y*4.0+iTimer,2.0)*PI;
+
+res = COMPAT_TEXTURE(Source,vTexCoord).rgb*RGBYUV;
+res.gb *= vec2(cos(phase),sin(phase));
+
+float signal = dot(vec3(1.0),res);
+
+FragColor.rgb = vec3(signal);
 }
 #endif
