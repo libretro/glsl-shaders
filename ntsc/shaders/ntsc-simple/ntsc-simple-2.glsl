a145baa2d5355c32f0a42f42e754cbd765c15dbf
metallic77 <43163462+metallic77@users.noreply.github.com>
2024-08-21 10:26:32 +0300

    Update ntsc-simple-2.glsl

--- ntsc/shaders/ntsc-simple/ntsc-simple-2.glsl
+++ ntsc/shaders/ntsc-simple/ntsc-simple-2.glsl
@@ -111,8 +111,8 @@ void main()
     if (system_choose == 4.0) system_clock = PAL_CLOCK/7.0;
     // c64 runs 4*PAL/NTSC then divides 18 or 14(ntsc), feeds the cpu freq, then runs 8x times of cpu
     if (system_choose == 5.0) system_clock = PAL_CLOCK/(PAL_CLOCK*4.0/18.0*8.0);
-    // Atari 2600
-    if (system_choose == 6.0) system_clock = 1.0; // A2600 is 1:1 ntsc clock
+    // Atari 2600 is 1:1 ntsc
+    if (system_choose == 6.0) system_clock = 3.579545*2.0; // stella outputs double pixels?
 
     float phase_alt = NTSC_CLOCK/system_clock;
     float v_phase_alt = phase_alt;
