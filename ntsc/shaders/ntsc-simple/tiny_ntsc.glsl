7357427a2d2c15223f896fc6045b0ab885d9a236
metallic77 <43163462+metallic77@users.noreply.github.com>
2024-08-22 15:03:31 +0300

    Update tiny_ntsc

--- ntsc/shaders/ntsc-simple/tiny_ntsc.glsl
+++ ntsc/shaders/ntsc-simple/tiny_ntsc.glsl
@@ -1,7 +1,8 @@
 #version 110
 
-#pragma parameter comb "Comb Filter" 0.7 0.0 1.0 0.05
+#pragma parameter comb "Comb Filter Strength" 0.8 0.0 1.0 0.05
 #pragma parameter ln_delay "NES/SNES Line Delay" 1.0 0.0 1.0 1.0
+#pragma parameter d_crawl "NES/SNES Dot Crawl" 1.0 0.0 1.0 1.0
 #pragma parameter sharp_pix "Sharper" 0.0 0.0 1.0 1.0
 
 #if defined(VERTEX)
@@ -27,6 +28,7 @@ COMPAT_ATTRIBUTE vec4 COLOR;
 COMPAT_ATTRIBUTE vec4 TexCoord;
 COMPAT_VARYING vec4 COL0;
 COMPAT_VARYING vec4 TEX0;
+COMPAT_VARYING vec2 ogl2pos;
 
 vec4 _oPosition1; 
 uniform mat4 MVPMatrix;
@@ -51,6 +53,7 @@ void main()
 {
     gl_Position = MVPMatrix * VertexCoord;
     TEX0.xy = TexCoord.xy*1.0001;
+    ogl2pos = TEX0.xy*TextureSize.xy;
 }
 
 #elif defined(FRAGMENT)
@@ -83,6 +86,7 @@ uniform COMPAT_PRECISION vec2 TextureSize;
 uniform COMPAT_PRECISION vec2 InputSize;
 uniform sampler2D Texture;
 COMPAT_VARYING vec4 TEX0;
+COMPAT_VARYING vec2 ogl2pos;
 
 // compatibility #defines
 #define vTexCoord TEX0.xy
@@ -94,12 +98,12 @@ COMPAT_VARYING vec4 TEX0;
 uniform COMPAT_PRECISION float comb;
 uniform COMPAT_PRECISION float ln_delay;
 uniform COMPAT_PRECISION float sharp_pix;
-
+uniform COMPAT_PRECISION float d_crawl;
 #else
 #define comb 0.6
 #define ln_delay 1.0
 #define sharp_pix 0.0
-
+#define d_crawl 1.0
 #endif
 
 #define PI   3.14159265358979323846
@@ -123,25 +127,24 @@ vec2 dxy = vec2(SourceSize.z*pixsize,0.0);
 vec2 dy = vec2(0.0,SourceSize.w*0.125);
 vec3 final = vec3(0.0);
 float sum = 0.0;
-int N = 2; 
-float timer = 0.0; if (InputSize.x < 300.0) timer = mod(float(FrameCount/2),2.0);
+float timer = 0.0; if (d_crawl == 1.0) timer = mod(float(FrameCount),3.0);
 
-for (int i=0; i<N; i++)
+for (int i=0; i<2; i++)
 {
 float n = float(i);
 float w = exp(-0.15*n*n); // gaussian low pass
 
 float line_delay = 0.0;
-if (ln_delay == 1.0) line_delay = vTexCoord.y*SourceSize.y; // snes line delay
+if (ln_delay == 1.0) line_delay = ogl2pos.y; // snes line delay
 //snes line delay deployed at 170/256, 2/3 of pi(180'), 120' or 1/3 lines as in real snes
-float phase = (vTexCoord.x*SourceSize.x + n - line_delay +timer )*PI*NTSC_RES/InputSize.x;
+float phase = (ogl2pos.x + n - line_delay +timer)*PI*NTSC_RES/InputSize.x;
 // comb filter line
 float phaseup = phase + PI;
 
-vec3 res = COMPAT_TEXTURE(Source,vTexCoord -dxy + dxy*n).rgb*RGBYUV;
+vec3 res   = COMPAT_TEXTURE(Source,vTexCoord -dxy + dxy*n).rgb*RGBYUV;
 vec3 resup = COMPAT_TEXTURE(Source,vTexCoord -dxy + dxy*n +dy).rgb*RGBYUV;
 
-vec3 carrier = vec3(1.0, sin(phase), cos(phase));
+vec3 carrier   = vec3(1.0, sin(phase  ), cos(phase));
 vec3 carrierup = vec3(1.0, sin(phaseup), cos(phaseup));
 
 res    *= carrier;
@@ -152,7 +155,7 @@ float lineup = dot(vec3(0.5),resup);
 // comb luma is line adding previous line, chroma is cancelled!
 float luma   = line + lineup;
 // comb chroma is line subtracting luma we already have!
-float chroma = line - luma/2.0*comb;
+float chroma = line - luma*0.5*comb;
 final.r += luma*w;
 final.gb += 2.0*vec2(chroma)*carrier.yz*w;
 sum += w;
