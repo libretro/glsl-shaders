16540029740bfda5fa033a1092a64ea9213a4286
metallic77 <43163462+metallic77@users.noreply.github.com>
2025-08-24 12:49:45 +0300

    re-write tiny_ntsc.glsl

--- ntsc/shaders/ntsc-simple/tiny_ntsc.glsl
+++ ntsc/shaders/ntsc-simple/tiny_ntsc.glsl
@@ -1,20 +1,20 @@
 #version 110
 
 /*
-Tiny-NTSC DariusG 2024-2025
+Tiny_NTSC by DariusG 2024-2025
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the Free
 Software Foundation; either version 2 of the License, or (at your option)
 any later version.
 */
-
-#pragma parameter comb "Comb Filter Strength" 0.8 0.0 1.0 0.05
-#pragma parameter c_lpf "Chroma Low Pass" 0.05 0.0 1.0 0.01
-#pragma parameter y_lpf "Luma Low Pass" 0.25 0.0 1.0 0.01
-#pragma parameter ln_delay "NES/SNES Line Delay" 1.0 0.0 1.0 1.0
-#pragma parameter d_crawl "NES/SNES Dot Crawl" 1.0 0.0 1.0 1.0
-#pragma parameter ntsc_sat "Saturation" 2.0 0.0 4.0 0.05
+#pragma parameter comb "Comb Filter Strength" 0.2 0.0 1.0 0.05
+#pragma parameter taps "Filter Taps (slower)" 6.0 3.0 12.0 1.0
+#pragma parameter lpf_w "Low Pass Width Y" 0.5 0.25 2.0 0.25
+#pragma parameter lpf_w_c "Low Pass Width C" 1.5 0.25 4.0 0.25
+#pragma parameter c_lpf "Chroma Low Pass (bleed)" 0.1 0.0 1.0 0.01
+#pragma parameter y_lpf "Luma Low Pass (sharpness)" 0.35 0.0 1.0 0.01
+#pragma parameter ntsc_sat "Saturation" 1.5 0.0 4.0 0.05
 
 #if defined(VERTEX)
 
@@ -39,7 +39,6 @@ COMPAT_ATTRIBUTE vec4 COLOR;
 COMPAT_ATTRIBUTE vec4 TexCoord;
 COMPAT_VARYING vec4 COL0;
 COMPAT_VARYING vec4 TEX0;
-COMPAT_VARYING vec2 ogl2pos;
 
 vec4 _oPosition1; 
 uniform mat4 MVPMatrix;
@@ -64,7 +63,6 @@ void main()
 {
     gl_Position = MVPMatrix * VertexCoord;
     TEX0.xy = TexCoord.xy*1.0001;
-    ogl2pos = TEX0.xy*TextureSize.xy;
 }
 
 #elif defined(FRAGMENT)
@@ -97,7 +95,6 @@ uniform COMPAT_PRECISION vec2 TextureSize;
 uniform COMPAT_PRECISION vec2 InputSize;
 uniform sampler2D Texture;
 COMPAT_VARYING vec4 TEX0;
-COMPAT_VARYING vec2 ogl2pos;
 
 // compatibility #defines
 #define vTexCoord TEX0.xy
@@ -107,22 +104,25 @@ COMPAT_VARYING vec2 ogl2pos;
 
 #ifdef PARAMETER_UNIFORM
 uniform COMPAT_PRECISION float comb;
-uniform COMPAT_PRECISION float ln_delay;
-uniform COMPAT_PRECISION float d_crawl;
-uniform COMPAT_PRECISION float ntsc_sat;
-uniform COMPAT_PRECISION float c_lpf;
+uniform COMPAT_PRECISION float taps;
+uniform COMPAT_PRECISION float lpf_w;
+uniform COMPAT_PRECISION float lpf_w_c;
 uniform COMPAT_PRECISION float y_lpf;
+uniform COMPAT_PRECISION float c_lpf;
+uniform COMPAT_PRECISION float ntsc_sat;
+
 #else
-#define comb 0.6
-#define ln_delay 1.0
-#define d_crawl 1.0
-#define ntsc_sat 1.0
-#define c_lpf 0.05
-#define y_lpf 0.35
+#define comb 0.3
+#define taps 5.0
+#define lpf_w 0.5
+#define lpf_w_c 1.0
+#define y_lpf 0.3
+#define c_lpf 0.5
+#define ntsc_sat 1.5
 #endif
 
-#define PI   3.14159265358979323846
-
+#define PI 3.1415926
+#define tau 6.2831852
 mat3 RGBYUV = mat3(0.299, 0.587, 0.114,
                         -0.299, -0.587, 0.886, 
                          0.701, -0.587, -0.114);
@@ -131,56 +131,67 @@ mat3 YUV2RGB = mat3(1.0, 0.0, 1.13983,
                           1.0, -0.39465, -0.58060,
                           1.0, 2.03211, 0.0);
 
-// max 170.666 color "dots" per line
-#define NTSC_RES 170.6666
+//detect if 256x224 or 320x224 and define phase
+float ph_2_3 = InputSize.x<300.0? 3.0 :2.0;
 
-void main() 
+float timer =  mod(float(FrameCount+1),ph_2_3);
+
+void main()
 {
-float pixsize = 0.5; 
-vec2 dxy = vec2(SourceSize.z*pixsize,0.0);
-vec2 dy = vec2(0.0,SourceSize.w*0.125);
-vec3 final = vec3(0.0);
-float sum = 0.0;
-float sumY = 0.0;
-float timer = 0.0; 
-if (d_crawl == 1.0) { InputSize.x<300.0? timer = mod(float(FrameCount+1),2.0) : timer = mod(float(FrameCount+1),3.0);}
-
-for (int i=-2; i<3; i++)
+    vec2 p = vec2(SourceSize.z*lpf_w,0.0);
+    vec2 pc = vec2(SourceSize.z*lpf_w_c,0.0);
+    vec2 y = vec2(0.0,SourceSize.w*0.25);
+    vec3 final = vec3(0.0);
+    float sum = 0.0;
+    float sumY = 0.0;
+    int steps = int(taps); 
+for (int i=-int(steps); i<int(steps+1); i++)
 {
-float n  = float(i);
-float w  = exp(-c_lpf*n*n); // Chroma low pass
-float wY = exp(-y_lpf*n*n); // Luma low pass
+    float n = float(i);    
+    float w  = exp(-c_lpf*n*n);
+    float wY = exp(-y_lpf*n*n);
+    // phase cycles every 3 pixels horizontally:
+    float phase_x = mod(vTexCoord.x*SourceSize.x, ph_2_3);
+// NTSC line-to-line subcarrier advance ≈ 0.5 cycles per line.
+    float phase_y =ph_2_3>2.0? mod(vTexCoord.y*SourceSize.y, 3.0) :0.0;
 
-float line_delay = 0.0;
-if (ln_delay == 1.0) line_delay = ogl2pos.y; // snes line delay
-//snes line delay deployed at 170/256, 2/3 of pi(180'), 120' or 1/3 lines as in real snes
-float phase = (ogl2pos.x + n - line_delay +timer)*PI*NTSC_RES/InputSize.x;
-// comb filter line
-float phaseup = phase + PI;
+// Combined horizontal+vertical phase in 3-phase space:
+    float phase = mod(phase_x - phase_y + timer + n, 3.0);
+    vec3 carrier   = vec3(1.0, cos(phase/3.0*tau ), sin(phase/3.0*tau));
+    vec3 carrierup = vec3(1.0, cos(phase/3.0*tau +PI), sin(phase/3.0*tau + PI));
+    
+    vec3 res   = COMPAT_TEXTURE(Source,vTexCoord + n*p).rgb*RGBYUV;
+    vec3 resc   = COMPAT_TEXTURE(Source,vTexCoord + n*pc).rgb*RGBYUV;
+    vec3 resup = COMPAT_TEXTURE(Source,vTexCoord + n*p -y).rgb*RGBYUV;
+    vec3 resupc = COMPAT_TEXTURE(Source,vTexCoord + n*pc -y).rgb*RGBYUV;
+    
+    res   *= carrier;
+    resc   *= carrier;
+    resup *= carrierup;
+    resupc *= carrierup;
 
-vec3 res   = COMPAT_TEXTURE(Source,vTexCoord -dxy + dxy*n).rgb*RGBYUV;
-vec3 resup = COMPAT_TEXTURE(Source,vTexCoord -dxy + dxy*n +dy).rgb*RGBYUV;
+    //get composite sample
+    float line = dot(res,vec3(0.5));
+    float linec = dot(resc,vec3(0.5));
+    float lineup = dot(resup,vec3(0.5));
+    float lineupc = dot(resupc,vec3(0.5));
+    // comb luma is line adding previous line, chroma is cancelled!
+    float luma   = line + lineup;
+    float lumac   = linec + lineupc;
+    // comb chroma is line subtracting luma we already have!
+    float chroma = linec - lumac*0.5*comb;
+    // lowpass Y and C, Luma has more bandwidth than Chroma (sharper)
+    if (i>-(steps/3) && i<(steps/3 + 1))
+    {
+    final.r  += luma*wY;
+    sumY += wY;
+    }
+    final.gb += vec2(chroma)*vec2(ntsc_sat,ntsc_sat*2.0)*carrier.yz*w;
 
-vec3 carrier   = vec3(1.0, sin(phase  ), cos(phase));
-vec3 carrierup = vec3(1.0, sin(phaseup), cos(phaseup));
-
-res    *= carrier;
-resup  *= carrierup;
-
-float line   = dot(vec3(0.5),res);
-float lineup = dot(vec3(0.5),resup);
-// comb luma is line adding previous line, chroma is cancelled!
-float luma   = line + lineup;
-// comb chroma is line subtracting luma we already have!
-float chroma = line - luma*0.5*comb;
-final.r += luma*wY;
-final.gb += ntsc_sat*vec2(chroma)*carrier.yz*w;
-sum += w;
-sumY += wY;
+    sum  += w;
 }
-
-final.gb /= sum;
-final.r /= sumY;
-FragColor.rgb = final*YUV2RGB;
+    final.r  /= sumY;
+    final.gb /= sum;
+    FragColor.rgb = final*YUV2RGB;
 }
 #endif
