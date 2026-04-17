903eb2058e52975b7e85ff4eabd2de19c4f60c3a
metallic77 <43163462+metallic77@users.noreply.github.com>
2026-04-17 17:01:54 +0300

    crt-pi revert to original

--- crt/shaders/crt-pi.glsl
+++ crt/shaders/crt-pi.glsl
@@ -29,12 +29,12 @@ MASK_TYPE defines what, if any, shadow mask to use. MASK_BRIGHTNESS defines how 
 
 #pragma parameter CURVATURE_X "Screen curvature - horizontal" 0.10 0.0 1.0 0.01
 #pragma parameter CURVATURE_Y "Screen curvature - vertical" 0.15 0.0 1.0 0.01
-#pragma parameter MASK_BRIGHTNESS "Mask brightness" 0.70 0.0 1.0 0.05
-#pragma parameter SCANLINE_WEIGHT "Scanline weight" 4.0 0.0 15.0 0.1
+#pragma parameter MASK_BRIGHTNESS "Mask brightness" 0.70 0.0 1.0 0.01
+#pragma parameter SCANLINE_WEIGHT "Scanline weight" 6.0 0.0 15.0 0.1
 #pragma parameter SCANLINE_GAP_BRIGHTNESS "Scanline gap brightness" 0.12 0.0 1.0 0.01
-#pragma parameter BLOOM_FACTOR "Bloom factor" 1.0 0.0 5.0 0.05
-#pragma parameter INPUT_GAMMA "Input gamma" 2.4 0.0 5.0 0.05
-#pragma parameter OUTPUT_GAMMA "Output gamma" 2.2 0.0 5.0 0.05
+#pragma parameter BLOOM_FACTOR "Bloom factor" 1.5 0.0 5.0 0.01
+#pragma parameter INPUT_GAMMA "Input gamma" 2.4 0.0 5.0 0.01
+#pragma parameter OUTPUT_GAMMA "Output gamma" 2.2 0.0 5.0 0.01
 
 // Haven't put these as parameters as it would slow the code down.
 #define SCANLINES
@@ -98,7 +98,7 @@ void main()
 	screenScale = TextureSize / InputSize;
 #endif
 	filterWidth = (InputSize.y / OutputSize.y) / 3.0;
-	TEX0 = TexCoord*1.0001;
+	TEX0 = TexCoord;
 	gl_Position = MVPMatrix * VertexCoord;
 }
 #elif defined(FRAGMENT)
@@ -186,6 +186,7 @@ void main()
 
 		vec3 colour = texture2D(Texture, tc).rgb;
 
+#if defined(SCANLINES)
 #if defined(GAMMA)
 #if defined(FAKE_GAMMA)
 		colour = colour * colour;
@@ -193,20 +194,29 @@ void main()
 		colour = pow(colour, vec3(INPUT_GAMMA));
 #endif
 #endif
+		scanLineWeight *= BLOOM_FACTOR;
+		colour *= scanLineWeight;
 
-// keep mask inside GAMMA to gain some brightness after gamma out
+#if defined(GAMMA)
+#if defined(FAKE_GAMMA)
+		colour = sqrt(colour);
+#else
+		colour = pow(colour, vec3(1.0/OUTPUT_GAMMA));
+#endif
+#endif
+#endif
 #if MASK_TYPE == 0
 		gl_FragColor = vec4(colour, 1.0);
 #else
 #if MASK_TYPE == 1
-		float whichMask = fract((gl_FragCoord.x*1.0001) * 0.5);
+		float whichMask = fract(gl_FragCoord.x * 0.5);
 		vec3 mask;
 		if (whichMask < 0.5)
 			mask = vec3(MASK_BRIGHTNESS, 1.0, MASK_BRIGHTNESS);
 		else
 			mask = vec3(1.0, MASK_BRIGHTNESS, 1.0);
 #elif MASK_TYPE == 2
-		float whichMask = fract((gl_FragCoord.x*1.0001) * 0.3333333);
+		float whichMask = fract(gl_FragCoord.x * 0.3333333);
 		vec3 mask = vec3(MASK_BRIGHTNESS, MASK_BRIGHTNESS, MASK_BRIGHTNESS);
 		if (whichMask < 0.3333333)
 			mask.x = 1.0;
@@ -216,21 +226,6 @@ void main()
 			mask.z = 1.0;
 #endif
 
-#if defined(GAMMA)
-	#if defined(FAKE_GAMMA)
-		colour = sqrt(colour);
-	#else
-		colour = pow(colour, vec3(1.0/OUTPUT_GAMMA));
-	#endif
-	
-#endif
-// Leave scanlines out of GAMMA, 
-// as it messes them (fract scanlines are TOO sensitive 
-// and should be 0.0 to 1.0 range)  
-#if defined(SCANLINES)
-		scanLineWeight *= BLOOM_FACTOR;
-		colour *= scanLineWeight;
-#endif
 		gl_FragColor = vec4(colour * mask, 1.0);
 #endif
 	}
