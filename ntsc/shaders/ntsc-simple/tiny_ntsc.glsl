e7e85d2016f9037611ebf314bf1efe554033a45c
metallic77 <43163462+metallic77@users.noreply.github.com>
2026-05-03 14:52:55 +0300

    Update tiny_ntsc.glsl

--- ntsc/shaders/ntsc-simple/tiny_ntsc.glsl
+++ ntsc/shaders/ntsc-simple/tiny_ntsc.glsl
@@ -1,7 +1,6 @@
 #version 110
-
 /*
-   TINY_NTSC DariusG 2025 
+   TINY_NTSC DariusG 2025-2026 
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -21,14 +20,11 @@
    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
    THE SOFTWARE.
 */
-#pragma parameter u_comb "Comb Filter Strength" 0.7 0.0 1.0 0.05
-#pragma parameter LPY "Luma Bandwidth" 1.3 0.0 2.0 0.02
-#pragma parameter LPC "Chroma Bandwidth" 0.1 0.0 1.0 0.01
-#pragma parameter c_gain "Chroma Gain" 1.5 0.0 3.0 0.05
-#pragma parameter d_crawl "Dot Crawl" 1.0 0.0 1.0 1.0
+// Parameter lines go here:
 
-#define PI 3.1415926
-#define TAU 6.2831852
+#define PI 3.14159
+
+
 #if defined(VERTEX)
 
 #if __VERSION__ >= 130
@@ -48,16 +44,12 @@
 #endif
 
 COMPAT_ATTRIBUTE vec4 VertexCoord;
-COMPAT_ATTRIBUTE vec4 COLOR;
 COMPAT_ATTRIBUTE vec4 TexCoord;
-COMPAT_VARYING vec4 COL0;
 COMPAT_VARYING vec4 TEX0;
 COMPAT_VARYING vec2 scale;
 COMPAT_VARYING vec2 invdims;
 COMPAT_VARYING vec2 ogl2pos;
-COMPAT_VARYING float maskpos;
 
-vec4 _oPosition1; 
 uniform mat4 MVPMatrix;
 uniform COMPAT_PRECISION int FrameDirection;
 uniform COMPAT_PRECISION int FrameCount;
@@ -70,33 +62,16 @@ uniform COMPAT_PRECISION vec2 InputSize;
 #define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
 #define OutSize vec4(OutputSize, 1.0 / OutputSize)
 
-#ifdef PARAMETER_UNIFORM
-uniform COMPAT_PRECISION float WHATEVER;
-#else
-#define WHATEVER 0.0
-#endif
-
 void main()
 {
-    gl_Position = MVPMatrix * VertexCoord;
-    TEX0.xy = TexCoord.xy*1.0001;
-    scale = SourceSize.xy/InputSize.xy;
-    ogl2pos = TEX0.xy*TextureSize;
-    maskpos = TEX0.x*OutputSize.x*scale.x;
-    invdims = 1.0/TextureSize;
+   gl_Position = MVPMatrix * VertexCoord;
+   TEX0.xy = TexCoord.xy*1.0001;
+   scale = SourceSize.xy/InputSize.xy;
+   invdims = 1.0/TextureSize.xy;
+   ogl2pos = TEX0.xy*TextureSize.xy;
 }
 
 #elif defined(FRAGMENT)
-
-#if __VERSION__ >= 130
-#define COMPAT_VARYING in
-#define COMPAT_TEXTURE texture
-out vec4 FragColor;
-#else
-#define COMPAT_VARYING varying
-#define FragColor gl_FragColor
-#define COMPAT_TEXTURE texture2D
-#endif
 
 #ifdef GL_ES
 #ifdef GL_FRAGMENT_PRECISION_HIGH
@@ -109,6 +84,16 @@ precision mediump float;
 #define COMPAT_PRECISION
 #endif
 
+#if __VERSION__ >= 130
+#define COMPAT_VARYING in
+#define COMPAT_TEXTURE texture
+out lowfp vec4 FragColor;
+#else
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
+#define COMPAT_TEXTURE texture2D
+#endif
+
 uniform COMPAT_PRECISION int FrameDirection;
 uniform COMPAT_PRECISION int FrameCount;
 uniform COMPAT_PRECISION vec2 OutputSize;
@@ -117,91 +102,103 @@ uniform COMPAT_PRECISION vec2 InputSize;
 uniform sampler2D Texture;
 COMPAT_VARYING vec4 TEX0;
 COMPAT_VARYING vec2 scale;
-COMPAT_VARYING float maskpos;
 COMPAT_VARYING vec2 invdims;
 COMPAT_VARYING vec2 ogl2pos;
 
 // compatibility #defines
-#define vTexCoord TEX0.xy
 #define Source Texture
+#define vTexCoord TEX0.xy
+#define iTime float(FrameCount)
+
 #define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
 #define OutSize vec4(OutputSize, 1.0 / OutputSize)
 
 #ifdef PARAMETER_UNIFORM
-uniform COMPAT_PRECISION float u_comb;
-uniform COMPAT_PRECISION float c_gain;
-uniform COMPAT_PRECISION float LPY;
-uniform COMPAT_PRECISION float LPC;
-uniform COMPAT_PRECISION float d_crawl;
+// All parameter floats need to have COMPAT_PRECISION in front of them
+uniform COMPAT_PRECISION float WARP;
 #else
-#define u_comb 0.7
-#define c_gain 1.5
-#define LPY 0.4
-#define LPC 0.2
-#define d_crawl 1.0
-
+#define WARP 0.04
 #endif
 
-#define phase_choose  (InputSize.x < 300.0 ? 3.0 :2.0)
-#define h_phase_choose  (InputSize.x < 300.0 ? 1.0 :2.0)
-#define gamma(c) c*c
+// we tell it to pass chroma "info" every four sample pixels at PI/2.0 (90 degrees)
+#define pi_two PI/2.0
+#define pi_four PI/4.0
 
-#define timer (d_crawl== 1.0? mod(float(FrameCount),2.0)*TAU/phase_choose : 0.0)
+vec3 rgb_yiq(vec3 color)
+{
+    float Y = 0.3*color.r+0.59*color.g+0.11*color.b;
+    float I = 0.6*color.r-0.275*color.g-0.32*color.b;
+    float Q = 0.21*color.r-0.52*color.g+0.31*color.b;
+    return vec3(Y,I,Q);
+}
+vec3 yiq_rgb(vec3 res){
+ /* --- Decode: YIQ -> RGB --- */
+    vec3 rgb;
+    float Y = res.r; float I = res.g; float Q= res.b;
+    rgb.r = Y + 0.956 * I + 0.621 * Q;
+    rgb.g = Y - 0.272 * I - 0.647 * Q;
+    rgb.b = Y - 1.106 * I + 1.703 * Q;
+return rgb;
+}
 
-vec3 rgb2yiq(vec3 rgb) {
-    float y = dot(rgb, vec3(0.299, 0.587, 0.114));
-    float i = dot(rgb, vec3(0.595716, -0.274453, -0.321263));
-    float q = dot(rgb, vec3(0.211456, -0.522591, 0.311135));
-    return vec3(y, i, q);
-}
-vec3 yiq2rgb(vec3 yiq) {
-    float r = yiq.x + 0.9563 * yiq.y + 0.6210 * yiq.z;
-    float g = yiq.x - 0.2721 * yiq.y - 0.6474 * yiq.z;
-    float b = yiq.x - 1.1070 * yiq.y + 1.7046 * yiq.z;
-    return vec3(r, g, b);
-}
+// "lines" in analogue is time actually, assuming clock 14.36 mhz
+// 14.36 mhz is 910 "lines" but need 2 pixels to carry chroma
+// 7.18 mhz are 455, maximum NTSC horiz. "resolution", 227,5 chroma "pixels".
+// part of 7.18 carries sound, h_sync etc.
+// total Y lines 6.0 mhz 2 chroma samples (14.36 clock)
+// Y 4.2 mhz ~ 0.66 of full x 4 = 6 approxim. 
+// a further part of it will be low passed to remove chroma
+// so actual Y is 3.2-3.4 mhz.
+// I 1.5 mhz ~ 1/5 of full, x 4 = 16 approxim.
+// Q 0.55 mhz ~ 1/11 of full, x 4 = 44 approxim.
+// keep them both ~1.0 mhz for speed
+// as color clock is 3.58, we got 4 "910" pixels sharing color at 14.36
 
 void main()
 {
+    vec2 dx = vec2(invdims.x,0.0);
     vec3 final = vec3(0.0);
     float sumY = 0.0;
-    float sumC = 0.0;
-    vec2 dx = vec2(invdims.x,0.0);
-    vec2 dy = vec2(0.0,invdims.y*0.25);
-    float line_phase =  mod(floor(ogl2pos.y),phase_choose)*TAU/phase_choose;
-// minimalistic 3 passes for performance
-    for (int i=-1; i<2; i++)
-    {
-        float n = float(i);
-        float wY = exp(-LPY*n*n);
-        float wC = exp(-LPC*n*n);
-        // we'll always have 170.666 color samples per line (3.579545 mhz)
-        // fix retroarch glsl vTexCoord [0.0,1.0] by multiplying "scale"
-        float phase = ((vTexCoord.x + n*invdims.x)*scale.x*170.666 )*TAU/h_phase_choose;
-        phase += timer;
-        phase += line_phase;
-        float cs = cos(phase);
-        float sn = sin(phase);
-        vec3 burst1 = vec3(1.0, cs, sn);
-        vec3 burst2 = vec3(1.0,-cs,-sn);
-        vec3 res1 = rgb2yiq(COMPAT_TEXTURE(Source,vTexCoord + n*dx).rgb); 
-        vec3 res2 = rgb2yiq(COMPAT_TEXTURE(Source,vTexCoord + n*dx - dy).rgb); 
-        res1 *= burst1;   
-        res2 *= burst2;   
-        float comp1 = dot(vec3(1.0),res1);
-        float comp2 = dot(vec3(1.0),res2);
-        // comb filter
-        float luma = (comp1+comp2)*0.5; // chroma cancelled!
-        final.r += luma*wY;
-        vec2 chroma = (comp1-luma*u_comb)*wC*burst1.gb*c_gain;
-        final.g += chroma.x;
-        final.b += chroma.y + chroma.x*0.15; // bleed some I to Q
+    float sumI = 0.0;
+    float sumQ = 0.0;
+// one frame is 29.97 in ntsc so iTime/2.0 as retroarch will do 60.    
+    float crawl = mod(iTime/2.0,4.0);
+    float pixel_x = floor(ogl2pos.x)*4.0;
+    float line_off = floor(ogl2pos.y)*2.0;
 
-        sumY += wY;
-        sumC += wC;
+// maximum Chroma blur width, assuming 1.0 mhz at 4x input scale
+// 6.0 mhz : 1.0 mhz * 4.0 scale = 24.0 samples sharing color
+// it could be 6 at 1x scale but blurry and undefined, not as in real analogue
+for (float x=-12.0; x<12.0; x=x+1.0)
+{
+   vec3 res = COMPAT_TEXTURE(Source,vTexCoord + dx*x).rgb;
+   vec3 yiq = rgb_yiq(res);
+// line_off: ntsc will alter phase every vertical line   
+   float angle = (pixel_x + x + crawl + line_off)*pi_two;
+   float cs = cos(angle);
+   float sn = sin(angle);
+   float hamm = cos(x*pi_four);
+// merge all in one "cable"
+   float signal = dot(vec3(yiq),vec3(1.0,cs,sn));
+// luma ~4.0 mhz is more crisp than chroma
+
+// hamming: 1.0-sharp + sharp*cos*(PI/4.0);
+   if(x>=-4.0 && x<4.0) { 
+    float wY = 0.75+0.25*hamm; sumY += wY;
+    final.r += signal*wY;
     }
-    final.r /= sumY;
-    final.gb /= sumC;
-    FragColor.rgb = yiq2rgb(final);
+
+    float wc = 0.9+0.1*hamm; 
+    // saturation paramater can be injected here
+    final.g += signal*cs*2.0*wc;
+    final.b += signal*sn*2.0*wc;
+    sumI += wc;
+    sumQ += wc;
 }
+   final.r /= sumY;
+   final.g /= sumI;
+   final.b /= sumQ;
+   
+   FragColor.rgb = yiq_rgb(final);
+} 
 #endif
